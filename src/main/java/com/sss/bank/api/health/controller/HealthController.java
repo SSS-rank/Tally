bbed1dab7c4cb08095f530961c7817067bb7186a
jungeunevepark <evecomcom98@naver.com>
2023-09-04 16:02:06 +0900

    :zap: feat: HealthController 생성
    Former-commit-id: 9a165d48dd370abb23bda1025b5f5f88873f0da7

---
+++ src/main/java/com/sss/bank/api/health/controller/HealthController.java
@@ -0,0 +1,27 @@
+package com.sss.bank.api.health.controller;
+
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sss.bank.api.health.dto.HealthCheckResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class HealthController {
+	private final Environment environment;
+
+	@GetMapping("/health")
+	public ResponseEntity<HealthCheckResponseDto> healthCheck() {
+		HealthCheckResponseDto healthCheckResponseDto = HealthCheckResponseDto.of("ok",
+			Arrays.asList(environment.getActiveProfiles()));
+		return ResponseEntity.ok(healthCheckResponseDto);
+	}
+}
