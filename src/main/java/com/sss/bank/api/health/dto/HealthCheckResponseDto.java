c49d31ee31c0de939e26852f29d3c4923a3da03c
jungeunevepark <evecomcom98@naver.com>
2023-09-04 16:01:36 +0900

    :zap: feat: HealthCheckResponseDto 정적 팩토리 메서드 생성
    Former-commit-id: 29e75baf57193735f18ac17f6c432e99606ccb4f

--- src/main/java/com/sss/bank/api/health/dto/HealthCheckResponseDto.java
+++ src/main/java/com/sss/bank/api/health/dto/HealthCheckResponseDto.java
@@ -10,4 +10,11 @@ import lombok.Getter;
 public class HealthCheckResponseDto {
 	private String health;
 	private List<String> activeProfiles;
+
+	public static HealthCheckResponseDto of(String health, List<String> activeProfiles) {
+		return HealthCheckResponseDto.builder()
+			.health(health)
+			.activeProfiles(activeProfiles)
+			.build();
+	}
 }
