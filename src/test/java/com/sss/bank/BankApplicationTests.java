f94d68dfa14745948da53c19b914139d5d9bfa4c
jungeunevepark <evecomcom98@naver.com>
2023-09-04 10:53:37 +0900

    :tada: init: 프로젝트 생성
    Former-commit-id: b436b3a594acae19705c1c8848cac6690b8755d6

---
+++ src/test/java/com/sss/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
