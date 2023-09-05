5ce2e61c0d6cd63b6e5a5f34a72543ff3f6e88f8
jungeunevepark <evecomcom98@naver.com>
2023-09-05 10:37:19 +0900

    :zap: feat: @Validated binding error가 발생할 경우 에러 처리하는 함수 생성
    Former-commit-id: 75d1384a1780ccf548fca486e98747e7b0da744c

---
+++ src/main/java/com/sss/bank/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sss.bank.global.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.sss.bank.global.error.dto.ErrorResponseDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(BindException.class)
+	protected ResponseEntity<ErrorResponseDto> handleBindException(BindException e) {
+		log.error("handleBindException", e);
+		ErrorResponseDto errorResponseDto = ErrorResponseDto.of(HttpStatus.BAD_REQUEST.toString(),
+			e.getBindingResult());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
+	}
+}
