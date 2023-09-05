7c5075994f36de0e3a88f6650620692cf150e910
박현철 <wns882@naver.com>
2023-09-05 17:23:14 +0900

    :zap: feat: TransferService 생성
    Former-commit-id: 1151a569b674d4aadf218a841e956123bdcee572

---
+++ src/main/java/com/sss/bank/domain/transfer/service/TransferService.java
@@ -0,0 +1,1 @@
+package com.sss.bank.domain.transfer.service;import com.sss.bank.domain.transfer.dto.TransferDepositReqDto;import com.sss.bank.domain.transfer.dto.TransferDepositRespDto;public interface TransferService {	TransferDepositRespDto createTransfer(TransferDepositReqDto transferDepositReqDto, long memberId);}
