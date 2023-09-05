d33ed75cd7af069cccea0ada6cb7adfeef12831d
rladbwls30 <rladbwls30@naver.com>
2023-09-05 14:55:49 +0900

    :zap: feat: token 에러처리 기능 설정
    Former-commit-id: 3d2f382242be4e9298e350199137b747955f1e07

--- src/main/java/com/sss/bank/global/error/ErrorCode.java
+++ src/main/java/com/sss/bank/global/error/ErrorCode.java
@@ -6,8 +6,11 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	// TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "businessExceptionTest")
-	;
+	// TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "businessExceptionTest");
+
+	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "001", "토큰이 만료되었습니다."),
+	NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "002", "해당 토큰은 유효하지 않습니다.");
+
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
@@ -18,5 +21,4 @@ public enum ErrorCode {
 	private HttpStatus httpStatus;
 	private String errorCode;
 	private String message;
-
 }
