a3400f9d7bccc9649ab08a140072f24c246bd038
박현철 <wns882@naver.com>
2023-09-05 17:21:12 +0900

    :zap: feat: AccountRepository 생성
    Former-commit-id: c37c1b9dc58ef4f6cc0d1f71efd8fc061dde053e

---
+++ src/main/java/com/sss/bank/domain/account/repository/AccountRepository.java
@@ -0,0 +1,1 @@
+package com.sss.bank.domain.account.repository;import org.springframework.data.jpa.repository.JpaRepository;import com.sss.bank.domain.account.entity.Account;public interface AccountRepository extends JpaRepository<Account, Long> {	Account findAccountByAccountNumberAndStatusIsFalse(String accountNum);}
