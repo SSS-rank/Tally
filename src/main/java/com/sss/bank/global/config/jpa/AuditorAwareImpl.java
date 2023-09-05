c260cab1f95029a9a9baa82ecdeff6995577c7af
jungeunevepark <evecomcom98@naver.com>
2023-09-05 12:38:56 +0900

    :bulb: comment: 함수 및 변수 주석 추가
    Former-commit-id: 1503d4627899cb58526fda6ab4bebb434d4d841e

--- src/main/java/com/sss/bank/global/config/jpa/AuditorAwareImpl.java
+++ src/main/java/com/sss/bank/global/config/jpa/AuditorAwareImpl.java
@@ -17,9 +17,13 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 	@Autowired
 	private HttpServletRequest httpServletRequest;
 
+	/*
+	 *  수정자의 정보를 담아 반환하는 함수
+	 *  수정자를 알수 없다면 unknown을 담아 보낸다.
+	 * */
 	@Override
 	public Optional<String> getCurrentAuditor() {
-		String modifiedBy = httpServletRequest.getRequestURI();
+		String modifiedBy = httpServletRequest.getRequestURI(); // 수정자의 정보를 담는다.
 		if (!StringUtils.hasText(modifiedBy)) {
 			modifiedBy = "unknown";
 		}
