9b153bc48f4a75220bcc1c3de9aa2e7d7a372d4e
jungeunevepark <evecomcom98@naver.com>
2023-09-05 10:07:12 +0900

    :sparkles: feat: Member Entity 생성
    Former-commit-id: 23093d6831cabb1cc1a7ef9891a7ae8638b1cee6

---
+++ src/main/java/com/sss/bank/domain/member/entity/Member.java
@@ -0,0 +1,46 @@
+package com.sss.bank.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
+
+	@Column(nullable = false, unique = true)
+	private String kakaoId;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String englishName;
+
+	@Column(nullable = false)
+	private String email;
+
+	private LocalDateTime withdrawalDate;
+
+	@CreatedDate
+	private LocalDateTime createDate;
+}
