03e84d4988ec7e3abce7d5a6f0a66873f17798b6
박현철 <wns882@naver.com>
2023-09-05 10:41:35 +0900

    :art: refactor: 개행을 CF에서 LF로 수정
    Former-commit-id: 6e218d24e3d38b588afb485d9404a2e92313292b

--- src/main/java/com/sss/bank/domain/account/entity/Account.java
+++ src/main/java/com/sss/bank/domain/account/entity/Account.java
@@ -1,1 +1,52 @@
-package com.sss.bank.domain.account.entity;import java.time.LocalDateTime;import javax.persistence.Column;import javax.persistence.Entity;import javax.persistence.FetchType;import javax.persistence.GeneratedValue;import javax.persistence.GenerationType;import javax.persistence.Id;import javax.persistence.JoinColumn;import javax.persistence.ManyToOne;import org.springframework.data.annotation.CreatedDate;import com.sss.bank.domain.member.entity.Member;import lombok.AccessLevel;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Getter;import lombok.NoArgsConstructor;@Entity@Getter@Builder@NoArgsConstructor(access = AccessLevel.PROTECTED)@AllArgsConstructorpublic class Account {	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)	private Long accountId;	@Column(nullable = false, unique = true)	private String accountUuid;	@JoinColumn(name = "member_id")	@ManyToOne(fetch = FetchType.LAZY)	private Member memberId;	@Column(nullable = false, unique = true)	private String accountNumber;	@Column(nullable = false)	private Long balance;	@Column(nullable = false)	private Boolean status;	@CreatedDate	private LocalDateTime createDate;}
+package com.sss.bank.domain.account.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.sss.bank.domain.member.entity.Member;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long accountId;
+
+	@Column(nullable = false, unique = true)
+	private String accountUuid;
+
+	@JoinColumn(name = "member_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member memberId;
+
+	@Column(nullable = false, unique = true)
+	private String accountNumber;
+
+	@Column(nullable = false)
+	private Long balance;
+
+	@Column(nullable = false)
+	private Boolean status;
+
+	@CreatedDate
+	private LocalDateTime createDate;
+}
--- src/main/java/com/sss/bank/domain/shop/entity/Shop.java
+++ src/main/java/com/sss/bank/domain/shop/entity/Shop.java
@@ -1,1 +1,31 @@
-package com.sss.bank.domain.shop.entity;import javax.persistence.Column;import javax.persistence.Entity;import javax.persistence.GeneratedValue;import javax.persistence.GenerationType;import javax.persistence.Id;import lombok.AccessLevel;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Getter;import lombok.NoArgsConstructor;@Entity@Getter@Builder@NoArgsConstructor(access = AccessLevel.PROTECTED)@AllArgsConstructorpublic class Shop {	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)	private Long shopId;	@Column(nullable = false)	private String shopType;	@Column(nullable = false)	private String shopName;}
+package com.sss.bank.domain.shop.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Shop {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long shopId;
+
+	@Column(nullable = false)
+	private String shopType;
+
+	@Column(nullable = false)
+	private String shopName;
+}
