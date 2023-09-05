10c43dc2ba00fbb401dabca2eadba18c3cf592e7
jungeunevepark <evecomcom98@naver.com>
2023-09-05 11:14:34 +0900

    :zap: feat: 지원하지 않는 Method를 호출할 경우 발생하는 에러를 처리하는 함수 생성
    Former-commit-id: 8060fa77dcb8c46bf2d7d999137a1d662af2b3b7

--- src/main/java/com/sss/bank/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/sss/bank/global/error/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.sss.bank.global.error.dto.ErrorResponseDto;
 
@@ -19,5 +20,14 @@ public class GlobalExceptionHandler {
 		ErrorResponseDto errorResponseDto = ErrorResponseDto.of(HttpStatus.BAD_REQUEST.toString(),
 			e.getBindingResult());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
+	}
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	protected ResponseEntity<ErrorResponseDto> handleMethodArgumentTypeMismatchException(
+		MethodArgumentTypeMismatchException e) {
+		log.error("hadleMethodArgumentTypeMismatchException", e);
+		ErrorResponseDto errorResponseDto = ErrorResponseDto.of(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(errorResponseDto);
 	}
 }
