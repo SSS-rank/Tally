cf4c815f0cc7b4703ceda8d8ba8ec1c283e46ed8
박현철 <wns882@naver.com>
2023-09-05 17:22:06 +0900

    :zap: feat: 계좌이체 응답 Dto 생성
    Former-commit-id: a094b3979276ed997e83e5ee8bff3f86c4b8e7cc

---
+++ src/main/java/com/sss/bank/domain/transfer/dto/TransferDepositRespDto.java
@@ -0,0 +1,1 @@
+package com.sss.bank.domain.transfer.dto;import lombok.AllArgsConstructor;import lombok.Getter;import lombok.NoArgsConstructor;@NoArgsConstructor@AllArgsConstructor@Getterpublic class TransferDepositRespDto {	private String receiverName;	private Long depositAmount;}
