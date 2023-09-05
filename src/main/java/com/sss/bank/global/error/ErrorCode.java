60d1a5d133c1e3d03bacd761fbef22011b898c1d
박현철 <wns882@naver.com>
2023-09-05 16:48:57 +0900

    :zap: feat: 계좌이체 기능 추가
    Former-commit-id: 2f1902373353632e8cfd78363d3c77870b28b7ac

--- src/main/java/com/sss/bank/global/error/ErrorCode.java
+++ src/main/java/com/sss/bank/global/error/ErrorCode.java
@@ -6,8 +6,14 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	// TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "businessExceptionTest")
-	;
+	// TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "businessExceptionTest");
+
+	INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "-1", "accessToken 정보가 잘못되었습니다."),
+	DUPLICATE_ACCOUNT(HttpStatus.BAD_REQUEST, "-2", "입 출금 계좌가 중복됩니다."),
+	INVALID_WITHDRAW_ACCOUNT(HttpStatus.BAD_REQUEST, "-3", "출금 계좌번호가 잘못되었습니다."),
+	INVALID_DEPOSIT_ACCOUNT(HttpStatus.BAD_REQUEST, "-4", "입금 계좌번호가 잘못되었습니다."),
+	UNAUTHORIZED_ACCESS(HttpStatus.BAD_REQUEST, "-5", "출금계좌 소유자와 accessToken이 일치하지 않습니다."),
+	INSUFFICIENT_FUNDS(HttpStatus.BAD_REQUEST, "-6", "잔액이 부족합니다");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
