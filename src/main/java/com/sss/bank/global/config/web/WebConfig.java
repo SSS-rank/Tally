cb9822971cdaf30df2db75ad632c3915b8f447eb
jungeunevepark <evecomcom98@naver.com>
2023-09-05 14:07:39 +0900

    :pencil2: fix: Cors 정책 캐싱 설정 제거
    Former-commit-id: 7b4329421438ce4ffcfc27a14132bbd04a9e4a72

--- src/main/java/com/sss/bank/global/config/web/WebConfig.java
+++ src/main/java/com/sss/bank/global/config/web/WebConfig.java
@@ -24,7 +24,6 @@ public class WebConfig implements WebMvcConfigurer {
 				HttpMethod.PATCH.name(),
 				HttpMethod.DELETE.name(),
 				HttpMethod.OPTIONS.name()
-			)
-			.maxAge(3600);
+			);
 	}
 }
