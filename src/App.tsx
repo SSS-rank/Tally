f7ce4af2d5a02d7856395ac889cf0e9c23b8d713
kosigi19 <kosigi19@naver.com>
2023-09-06 17:09:02 +0900

    ✨ feat: 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Layout from './components/Layout/Layout';
 import AccountAdd from './pages/AccountAdd';
 import AccountDetail from './pages/AccountDetail';
 import MainPage from './pages/MainPage';
@@ -9,14 +10,16 @@ import TransferPage from './pages/TransferPage';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route path="/accountadd" element={<AccountAdd />} />
-				<Route path="/accountdetail/:accountNumber" element={<AccountDetail />} />
-				<Route path="/transfer" element={<TransferPage />} />
-			</Routes>
-		</BrowserRouter>
+		<Layout>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/accountadd" element={<AccountAdd />} />
+					<Route path="/accountdetail/:accountNumber" element={<AccountDetail />} />
+					<Route path="/transfer" element={<TransferPage />} />
+				</Routes>
+			</BrowserRouter>
+		</Layout>
 	);
 }
 
