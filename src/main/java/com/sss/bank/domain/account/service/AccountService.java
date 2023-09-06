7f5e15f07d69a6c7aff890e321498959601f7beb
박현철 <wns882@naver.com>
2023-09-06 10:49:26 +0900

    :zap: feat: accountService 생성
    Former-commit-id: d830a69f27aaf0f9aa6d92fe9fec037edd0b4481

---
+++ src/main/java/com/sss/bank/domain/account/service/AccountService.java
@@ -0,0 +1,4 @@
+package com.sss.bank.domain.account.service;
+
+public interface AccountService {
+}
---
+++ src/main/java/com/sss/bank/domain/account/service/AccountServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sss.bank.domain.account.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class AccountServiceImpl implements AccountService {
+}
