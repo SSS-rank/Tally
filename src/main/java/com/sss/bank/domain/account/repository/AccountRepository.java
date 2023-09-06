5392c1966e3342a909b2c4e4b6dfbfae4ce732de
박현철 <wns882@naver.com>
2023-09-06 14:46:45 +0900

    :zap: feat: AccountRepository Account 테이블 CountRow 메서드 추가
    Former-commit-id: 1d45de4a7f6523d5788a64724e305a2fcd3c922b

--- src/main/java/com/sss/bank/domain/account/repository/AccountRepository.java
+++ src/main/java/com/sss/bank/domain/account/repository/AccountRepository.java
@@ -1,9 +1,15 @@
 package com.sss.bank.domain.account.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sss.bank.domain.account.entity.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	Account findAccountByAccountNumberAndStatusIsFalse(String accountNum);
+
+	@Query("SELECT COUNT(a) FROM Account a")
+	Optional<Integer> countRows();
 }
