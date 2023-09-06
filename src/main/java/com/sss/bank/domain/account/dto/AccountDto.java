e93a7a1012da4492547647e405ccf5f8543319f8
박현철 <wns882@naver.com>
2023-09-06 14:46:17 +0900

    :zap: feat: AccountDto 추가 및 Create Dto 추가
    Former-commit-id: 36edffe81ccf0dd849a3ff6b3d9ba08714ba32b5

--- src/main/java/com/sss/bank/domain/account/dto/AccountDto.java
+++ src/main/java/com/sss/bank/domain/account/dto/AccountDto.java
@@ -1,5 +1,38 @@
 package com.sss.bank.domain.account.dto;
 
+import javax.validation.constraints.NotNull;
+
+import com.sss.bank.domain.account.entity.Account;
+import com.sss.bank.domain.bank.entity.Bank;
+import com.sss.bank.domain.member.entity.Member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class AccountDto {
 
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Getter
+	public static class AccountCreateRequestDto {
+
+		@NotNull
+		private String bankCode;
+		@NotNull
+		private String accountPassword;
+
+		public Account toAccountEntity(Member member, String accountNum, String uuid, Bank bank) {
+			//	String accountNum = generateAccountNum(dbValue);
+			return Account.builder()
+				.accountUuid(uuid)
+				.accountNumber(accountNum)
+				.balance(1000000L)
+				.memberId(member)
+				.status(false)
+				.password(this.accountPassword)
+				.bankId(bank)
+				.build();
+		}
+	}
 }
