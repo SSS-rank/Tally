f6336e9b72a5e6ee11472757f45f9502e115fefe
rladbwls30 <rladbwls30@naver.com>
2023-09-06 17:04:52 +0900

    :zap: feat: 카카오에서 가져온 회원 정보 저장하는 ServiceImpl 생성
    Former-commit-id: 1e14ea689faec68dec1d558927d2f8277225e3e1

---
+++ src/main/java/com/sss/bank/external/oauth/kakao/service/KakaoLoginApiServiceImpl.java
@@ -0,0 +1,36 @@
+package com.sss.bank.external.oauth.kakao.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import com.sss.bank.external.oauth.kakao.client.KakaoUserInfoClient;
+import com.sss.bank.external.oauth.kakao.dto.KakaoUserInfoResDto;
+import com.sss.bank.external.oauth.model.OAuthAttributes;
+import com.sss.bank.external.oauth.service.SocialLoginApiService;
+import com.sss.bank.global.jwt.constant.GrantType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class KakaoLoginApiServiceImpl implements SocialLoginApiService {
+	private final KakaoUserInfoClient kakaoUserInfoClient;
+	private final String CONTENT_TYPE = "application/x-www-form-urlencoded;charset=utf-8";
+
+	@Override
+	public OAuthAttributes getUserInfo(String accessToken) {
+		KakaoUserInfoResDto kakaoUserInfoResDto = kakaoUserInfoClient.getKakaoUserInfo(CONTENT_TYPE, GrantType.BEARER.getType()+" "+accessToken);
+		KakaoUserInfoResDto.KakaoAccount kakaoAccount = kakaoUserInfoResDto.getKakaoAccount();
+		String email = kakaoAccount.getEmail();
+
+		return OAuthAttributes.builder()
+			.kakaoId(kakaoUserInfoResDto.getId())
+			.name(kakaoAccount.getProfile().getNickname())
+			.email(!StringUtils.hasText(email) ? null : email)
+			.build();
+	}
+}
