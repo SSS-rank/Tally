fdfed519c471c52ac6997cf11fa20fc727ceb2de
jungeunevepark <evecomcom98@naver.com>
2023-09-07 13:59:25 +0900

    :zap: feat: ShopRepository가 JpaRepository를 상속하도록 구현
    Former-commit-id: bb1b2d6e2ccaf0c534044582bc0d475855a99b39

--- src/main/java/com/sss/bank/domain/shop/repository/ShopRepository.java
+++ src/main/java/com/sss/bank/domain/shop/repository/ShopRepository.java
@@ -3,12 +3,13 @@ package com.sss.bank.domain.shop.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sss.bank.domain.shop.entity.Shop;
 
 @Repository
-public interface ShopRepository {
+public interface ShopRepository extends JpaRepository<Shop, Long> {
 	Optional<Shop> findShopByShopId(Long shopId);
 
 	List<Shop> findAllByShopType(int shopType);
