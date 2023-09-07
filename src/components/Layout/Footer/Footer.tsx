8a3db93b836262c777767ff5cc61f9b0246f05e7
kosigi19 <kosigi19@naver.com>
2023-09-07 15:48:03 +0900

    ✨ feat: header, footer 수정

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
+	const footerStyle: React.CSSProperties = {
+		textAlign: 'center',
+		lineHeight: '100px',
+		backgroundColor: '#91C0EB',
+	};
+
 	return (
-		<footer>
-			<h2> This is Footer</h2>
+		<footer style={footerStyle}>
+			Copyright 2023. <strong>Team A108 SSS</strong>. All Rights Reserved.
 		</footer>
 	);
 };
