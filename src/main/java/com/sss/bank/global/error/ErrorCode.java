d162164c298cbb0364e12228281115bc181fac74
jungeunevepark <evecomcom98@naver.com>
2023-09-07 14:37:48 +0900

    :zap: feat: shop이 존재하지 않을 때, 보내는 ErrorCode 생성
    Former-commit-id: 3d79f79b55567e08138270f3b3a778df8ba4aa1e

--- src/main/java/com/sss/bank/global/error/ErrorCode.java
+++ src/main/java/com/sss/bank/global/error/ErrorCode.java
@@ -8,17 +8,20 @@ import lombok.Getter;
 public enum ErrorCode {
 	// TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "businessExceptionTest");
 
+	// 계좌
 	INVALID_ACCESS_TOKEN(HttpStatus.BAD_REQUEST, "-1", "accessToken 정보가 잘못되었습니다."),
 	DUPLICATE_ACCOUNT(HttpStatus.BAD_REQUEST, "-2", "입 출금 계좌가 중복됩니다."),
 	INVALID_WITHDRAW_ACCOUNT(HttpStatus.BAD_REQUEST, "-3", "출금 계좌번호가 잘못되었습니다."),
 	INVALID_DEPOSIT_ACCOUNT(HttpStatus.BAD_REQUEST, "-4", "입금 계좌번호가 잘못되었습니다."),
 	UNAUTHORIZED_ACCESS(HttpStatus.BAD_REQUEST, "-5", "출금계좌 소유자와 accessToken이 일치하지 않습니다."),
 	INSUFFICIENT_FUNDS(HttpStatus.BAD_REQUEST, "-6", "잔액이 부족합니다"),
-	// TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "businessExceptionTest");
 
+	// 인증 & 인가
 	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "001", "토큰이 만료되었습니다."),
-	NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "002", "해당 토큰은 유효하지 않습니다.");
+	NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "002", "해당 토큰은 유효하지 않습니다."),
 
+	// 가게
+	NOT_EXIST_SHOP(HttpStatus.BAD_REQUEST, "010", "해당 가게는 존재하지 않습니다.");
 
 	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
