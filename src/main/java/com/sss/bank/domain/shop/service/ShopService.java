e61ff22721fb373414d2e63915a03eefd6c2713d
jungeunevepark <evecomcom98@naver.com>
2023-09-07 13:36:11 +0900

    :zap: feat: ShopService 생성 및 함수 선언
    Former-commit-id: 710a9ff48094c837045c7d3d53c507f65074d841

---
+++ src/main/java/com/sss/bank/domain/shop/service/ShopService.java
@@ -0,0 +1,7 @@
+package com.sss.bank.domain.shop.service;
+
+import com.sss.bank.api.shop.dto.ShopDto;
+
+public interface ShopService {
+	ShopDto.ShopRespDto createShop(ShopDto.ShopReqDto shopReqDto);
+}
