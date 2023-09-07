a1658c4012e33600a62deb005b6fd86fe68b80ac
jungeunevepark <evecomcom98@naver.com>
2023-09-07 13:59:38 +0900

    :zap: feat: Shop 정보를 저장하는 함수 구현
    Former-commit-id: 2579da878f06a7c674f595a9be284a01af426d76

--- src/main/java/com/sss/bank/domain/shop/service/ShopServiceImpl.java
+++ src/main/java/com/sss/bank/domain/shop/service/ShopServiceImpl.java
@@ -1,16 +1,24 @@
 package com.sss.bank.domain.shop.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sss.bank.api.shop.dto.ShopDto;
+import com.sss.bank.domain.shop.entity.Shop;
+import com.sss.bank.domain.shop.repository.ShopRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ShopServiceImpl implements ShopService {
+	private final ShopRepository shopRepository;
+
 	@Override
 	public ShopDto.ShopRespDto createShop(ShopDto.ShopReqDto shopReqDto) {
-		return null;
+		Shop shop = Shop.from(shopReqDto);
+		shopRepository.save(shop);
+		return ShopDto.ShopRespDto.from(shop);
 	}
 }
