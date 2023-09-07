c3ed9b9fc29bee869fda654b03d0b9ce2f164750
박현철 <wns882@naver.com>
2023-09-07 15:02:05 +0900

    :zap: feat: 잔액 가져오기 Dto
    Former-commit-id: d4da30d70be438ad6e0cfb02c35edf5823948054

--- src/main/java/com/sss/bank/api/account/dto/AccountDto.java
+++ src/main/java/com/sss/bank/api/account/dto/AccountDto.java
@@ -4,7 +4,10 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.sss.bank.domain.account.entity.Account;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -37,4 +40,43 @@ public class AccountDto {
 		@NotNull
 		private String accountPassword;
 	}
+
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Getter
+	public static class AccountGetBalanceReqDto {
+
+		@NotNull
+		@NotEmpty
+		@Size(max = 5)
+		private String accountPassword;
+
+		@NotNull
+		@NotEmpty
+		@Size(max = 20)
+		private String accountNum;
+
+	}
+
+	@Builder
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Getter
+	public static class AccountGetBalanceRespDto {
+
+		private String bankName;
+
+		private String accountNumber;
+
+		private Long balance;
+
+		public static AccountGetBalanceRespDto of(Account account) {
+			return AccountGetBalanceRespDto.builder()
+				.balance(account.getBalance())
+				.bankName(account.getBankId().getBankName())
+				.accountNumber(account.getAccountNumber())
+				.build();
+		}
+	}
+
 }
