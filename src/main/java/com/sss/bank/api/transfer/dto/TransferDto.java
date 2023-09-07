a58c90b3c485a3f52d28a8cfebf51968c276cd79
박현철 <wns882@naver.com>
2023-09-07 13:45:28 +0900

    :zap: feat: 거래목록 조회 RequestDto 및 ResponseDto 추가
    Former-commit-id: 9c772f95e255de53c529b3584e727b4349269dff

--- src/main/java/com/sss/bank/api/transfer/dto/TransferDto.java
+++ src/main/java/com/sss/bank/api/transfer/dto/TransferDto.java
@@ -1,12 +1,19 @@
 package com.sss.bank.api.transfer.dto;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sss.bank.domain.transfer.entity.Transfer;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,6 +39,12 @@ public class TransferDto {
 		@Min(1)
 		private Long depositAmount;
 
+		@Column
+		private String withdrawAccountContent;
+
+		@Column
+		private String depositAccountContent;
+
 	}
 
 	@NoArgsConstructor
@@ -42,6 +55,57 @@ public class TransferDto {
 		private String receiverName;
 
 		private Long depositAmount;
+
+	}
+
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Getter
+	public static class TransferListReqDto {
+
+		@NotNull
+		@NotEmpty
+		@Size(max = 20)
+		private String accountNum;
+
+		@NotNull
+		private String accountPassword;
+
+		@NotNull
+		private int page;
+
+		@NotNull
+		private int limit;
+
+	}
+
+	@NoArgsConstructor
+	@Builder
+	@AllArgsConstructor
+	@Getter
+	public static class TransferListRespDto {
+
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+		private LocalDateTime transferDate;
+
+		private String flag;
+
+		private Long amount;
+
+		private String content;
+
+		private String transferUuid;
+
+		public static TransferListRespDto of(Transfer transfer, String flag) {
+			return TransferListRespDto
+				.builder()
+				.transferDate(transfer.getTransferDate())
+				.flag(flag)
+				.content(transfer.getDepositAccountContent())
+				.amount(transfer.getAmount())
+				.transferUuid(transfer.getTransferUuid())
+				.build();
+		}
 
 	}
 }
