a328059901ad557eac8e9261c2c6ba131a1aaba5
박현철 <wns882@naver.com>
2023-09-07 13:45:39 +0900

    :zap: feat: 거래목록 조회 Repository추가
    Former-commit-id: 33bc1cb45fcde44101f4323072cc58e9702bab62

--- src/main/java/com/sss/bank/domain/transfer/repository/TransferRepository.java
+++ src/main/java/com/sss/bank/domain/transfer/repository/TransferRepository.java
@@ -1,9 +1,18 @@
 package com.sss.bank.domain.transfer.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.sss.bank.domain.account.entity.Account;
 import com.sss.bank.domain.transfer.entity.Transfer;
 
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
-	
+
+	@Query("SELECT t FROM Transfer t WHERE t.sender = :account OR t.receiver = :account")
+	Page<Transfer> findTransfersByDepositAccountIdOrWithdrawAccountId(@Param("account") Account account,
+		Pageable pageable);
+
 }
