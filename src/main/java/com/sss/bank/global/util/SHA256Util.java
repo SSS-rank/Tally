9c7493c7e6593a791932b0bd1dde07ea09e1960a
jungeunevepark <evecomcom98@naver.com>
2023-09-08 14:57:53 +0900

    :sparkles: feat: 비밀번호 난수 처리를 하는 SHA256Util 생성
    Former-commit-id: 2c9560555b6c1494d6dd3b9d8d8fe33f0d97b5cb

---
+++ src/main/java/com/sss/bank/global/util/SHA256Util.java
@@ -0,0 +1,27 @@
+package com.sss.bank.global.util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SHA256Util {
+
+	public static String createSalt() {
+		SecureRandom random = new SecureRandom();
+
+		// 랜덤 8 바이트 base64 인코딩
+		byte[] bytes = new byte[8];
+		random.nextBytes(bytes);
+
+		return new String(Base64.getEncoder().encode(bytes));
+	}
+
+	public static String getEncrypt(String password, String salt) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update((password + salt).getBytes());
+		String hex = String.format("%064x", new BigInteger(1, md.digest()));
+		return hex;
+	}
+}
