b4ad50b1644bef8fd39c389c352d6c815e497c37
rladbwls30 <rladbwls30@naver.com>
2023-09-11 14:39:18 +0900

    :zap: feat: swagger에 Header 입력 추가
    Former-commit-id: 6eb45934233b5c9cf208e9a9e5fb4a6b4a114381

--- src/main/java/com/sss/bank/global/config/SwaggerConfig.java
+++ src/main/java/com/sss/bank/global/config/SwaggerConfig.java
@@ -1,13 +1,26 @@
 package com.sss.bank.global.config;
 
+
+import java.net.http.HttpHeaders;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -18,6 +31,8 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.OAS_30)
 			.useDefaultResponseMessages(true)
+			.securityContexts(List.of(this.securityContext()))
+			.securitySchemes(List.of(this.apiKey()))
 			.apiInfo(apiInfo())
 			.select()
 			.apis(RequestHandlerSelectors.basePackage("com.sss.bank.api"))
@@ -25,6 +40,21 @@ public class SwaggerConfig {
 			.build();
 	}
 
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+			.securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return List.of(new SecurityReference("Authorization", authorizationScopes));
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "Bearer", "Header");
+	}
 	public ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 			.title("SSS-Bank API Documentation")
