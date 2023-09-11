efd638a9032e228c09c5210b74f4c2dcbb1e117a
jungeunevepark <evecomcom98@naver.com>
2023-09-11 13:01:20 +0900

    :zap: feat: 유저의 계좌 리스트를 불러오는 API 구현
    Former-commit-id: 0b3d71a2345ecdcda07efdf020e9e0290ef3769b

--- src/main/java/com/sss/bank/api/account/controller/AccountController.java
+++ src/main/java/com/sss/bank/api/account/controller/AccountController.java
@@ -1,20 +1,25 @@
 package com.sss.bank.api.account.controller;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sss.bank.api.account.dto.AccountDto;
 import com.sss.bank.domain.account.service.AccountService;
+import com.sss.bank.global.resolver.MemberInfo;
+import com.sss.bank.global.resolver.MemberInfoDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -54,5 +59,12 @@ public class AccountController {
 
 		return ResponseEntity.status(HttpStatus.OK).body(accountGetBalanceRespDto);
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<AccountDto>> getAccountList(@RequestParam String code,
+		@MemberInfo MemberInfoDto memberInfoDto) {
+		List<AccountDto> accountList = accountService.getAccountList(memberInfoDto, code);
+		return ResponseEntity.status(HttpStatus.OK).body(accountList);
 	}
 }
