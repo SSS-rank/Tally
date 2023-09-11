ccca59d48ee624bb5670075914eb5f06d16e0203
박현철 <wns882@naver.com>
2023-09-11 16:55:35 +0900

    :sparkles: feat: Stamp Entity 생성
    Former-commit-id: a86b0d524b25800e8162b79f212e2358b3fe1ac3

--- backend/src/main/java/com/sss/tally/domain/stamp/entity/Stamp.java
+++ backend/src/main/java/com/sss/tally/domain/stamp/entity/Stamp.java
@@ -1,35 +1,48 @@
 package com.sss.tally.domain.stamp.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import com.sss.tally.domain.member.entity.Member;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Builder
 public class Stamp {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long stampId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long stampId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member memberId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member memberId;
+	@Column(nullable = false)
+	private Long travelLocation;
 
-    private Long travelLocation;
+	@Column(nullable = false)
+	private int travelType;
 
-    @Column(nullable = false)
-    private int travelType;
-
-    @CreatedDate
-    private LocalDateTime createDate;
+	@CreatedDate
+	private LocalDateTime createDate;
 }
