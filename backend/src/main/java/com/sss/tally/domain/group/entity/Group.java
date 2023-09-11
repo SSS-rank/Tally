68ade56ed5c747200288f318341298d8ef47c9aa
박현철 <wns882@naver.com>
2023-09-11 16:54:37 +0900

    :sparkles: feat: Group Entity 생성
    Former-commit-id: 16691dca59db9a9b179bc2eb792425ad9da053a1

---
+++ backend/src/main/java/com/sss/tally/domain/group/entity/Group.java
@@ -0,0 +1,42 @@
+package com.sss.tally.domain.group.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.sss.tally.domain.member.entity.Member;
+import com.sss.tally.domain.travel.entity.Travel;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Builder
+public class Group {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long groupId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member memberId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "travel_id")
+	private Travel travelId;
+
+}
