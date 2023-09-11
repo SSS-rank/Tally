f5f65d512ad8d595db9c109601ea04d96e30cc2c
박현철 <wns882@naver.com>
2023-09-11 16:54:09 +0900

    :sparkles: feat: CustomChecklist Entity 생성
    Former-commit-id: 7bf2400f27cb74dd0b976c6bfbba6ee5e68fc74b

--- backend/src/main/java/com/sss/tally/domain/customchecklist/entity/CustomChecklist.java
+++ backend/src/main/java/com/sss/tally/domain/customchecklist/entity/CustomChecklist.java
@@ -3,6 +3,8 @@ package com.sss.tally.domain.customchecklist.entity;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,9 +13,23 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.sss.tally.domain.member.entity.Member;
+import com.sss.tally.domain.travel.entity.Travel;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class CustomChecklist {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +39,9 @@ public class CustomChecklist {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Member memberId;
 
-	private Long travelId;
+	@JoinColumn(name = "travel_id")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Travel travelId;
 
 	@Column(nullable = false)
 	private String content;
