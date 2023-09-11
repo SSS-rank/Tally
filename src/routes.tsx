58c23d446e224eff8417ec8124f667687135b04d
zero9657@naver.com <zero9657@naver.com>
2023-09-11 14:20:44 +0900

    ✏️ fix : shopList 컴포넌트 이름 변경

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import AccountAdd from './pages/AccountAdd';
 import AccountDetail from './pages/AccountDetail';
 import AccountList from './pages/AccountList';
 import MainPage from './pages/MainPage';
-import ShopPage from './pages/Shop/ShopList';
+import ShopList from './pages/Shop/ShopList';
 import TransferPage from './pages/TransferPage';
 
 function Routes() {
@@ -21,7 +21,7 @@ function Routes() {
 		},
 		{
 			path: '/shop',
-			element: <ShopPage />,
+			element: <ShopList />,
 		},
 		{
 			path: '/add',
