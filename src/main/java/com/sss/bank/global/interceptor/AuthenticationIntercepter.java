794cdc0f4eaa8dd257e831e4d3460b670313e01b
jungeunevepark <evecomcom98@naver.com>
2023-09-11 16:30:47 +0900

    :bug: fix: react에서 preflignt를 차단하는 현상 수정
    Former-commit-id: fd2dd05f02b87eafc9b2985693dce1022dad4c7e

--- src/main/java/com/sss/bank/global/interceptor/AuthenticationIntercepter.java
+++ src/main/java/com/sss/bank/global/interceptor/AuthenticationIntercepter.java
@@ -3,6 +3,7 @@ package com.sss.bank.global.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -24,6 +25,10 @@ public class AuthenticationIntercepter implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
 		Exception {
+		// 0. preflignt면 무시
+		if (HttpMethod.OPTIONS.name().equals(request.getMethod())) {
+			return true;
+		}
 
 		String swaggerUri = request.getRequestURI();
 		if (swaggerUri.contains("swagger") || swaggerUri.contains("api-docs") || swaggerUri.contains("webjars"))
