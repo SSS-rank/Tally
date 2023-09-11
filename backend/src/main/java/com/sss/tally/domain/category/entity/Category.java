2ca16ec5b668a77c4501fbb1e1a389fcc534cd31
박현철 <wns882@naver.com>
2023-09-11 16:53:41 +0900

    :sparkles: feat: Categoryr Entity 생성
    Former-commit-id: a409e26ef8d15dd45a53e9c874adfecf69fb77a2

---
+++ backend/src/main/java/com/sss/tally/domain/category/entity/Category.java
@@ -0,0 +1,32 @@
+package com.sss.tally.domain.category.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Builder
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long categoryId;
+
+	@Column(nullable = false)
+	private String categoryType;
+
+}
