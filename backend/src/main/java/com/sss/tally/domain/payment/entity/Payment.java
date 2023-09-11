7a84ec53d9b6d344fa6aeb39ecbce633bfc997ab
박현철 <wns882@naver.com>
2023-09-11 16:55:19 +0900

    :sparkles: feat: Payment Entity 생성
    Former-commit-id: d84228f8e4bb5ca537b1a4c32be13dee4ead83b1

---
+++ backend/src/main/java/com/sss/tally/domain/payment/entity/Payment.java
@@ -0,0 +1,81 @@
+package com.sss.tally.domain.payment.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.sss.tally.domain.account.entity.Account;
+import com.sss.tally.domain.category.entity.Category;
+import com.sss.tally.domain.member.entity.Member;
+import com.sss.tally.domain.travel.entity.Travel;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long paymentId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member memberId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "account_id")
+	private Account accountId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "travel_id")
+	private Travel travelId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	private Category categoryId;
+
+	@Column(nullable = false)
+	private Long amount;
+
+	@Column(nullable = false)
+	private LocalDateTime paymentLocalDate;
+
+	@CreatedDate
+	@Column(nullable = false)
+	private LocalDateTime paymentKoreaDate;
+
+	@Column(nullable = false)
+	private String paymentMemo;
+
+	@Column(nullable = false)
+	private PaymentMethodEnum paymentMethod;
+
+	@Column(nullable = false)
+	private Boolean visible;
+
+	@Column(nullable = false)
+	private Boolean status;
+
+	@Column(nullable = false)
+	private CalculateStatusEnum calculateStatus;
+
+}
