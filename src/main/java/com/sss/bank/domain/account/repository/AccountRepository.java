e99cab0ab4c41f6998e3a0cdfb242d6d4588f732
jungeunevepark <evecomcom98@naver.com>
2023-09-11 12:59:06 +0900

    :zap: feat: 동일한 뱅크 코드를 가진 유저의 모든 계좌를 반환하는 JPA 함수 선언
    Former-commit-id: 7e650d56e4996a92407cf21d2144c08329331243

--- src/main/java/com/sss/bank/domain/account/repository/AccountRepository.java
+++ src/main/java/com/sss/bank/domain/account/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.sss.bank.domain.account.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
 	@Query("SELECT COUNT(a) FROM Account a")
 	Optional<Integer> countAccountRows();
+
+	List<Account> findAllByMemberId_MemberIdAndBankId_BankIdAndStatusIsFalse
+		(Long memberId, Long bankId);
 }
