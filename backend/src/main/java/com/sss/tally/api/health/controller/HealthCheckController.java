5df76cc6700b683ae8b268f718af0b8b0fa6f311
rladbwls30 <rladbwls30@naver.com>
2023-09-11 16:59:31 +0900

    :zap: feat: HealthCheckController 생성
    Former-commit-id: c08be0fba567f6d6a7278af23a0d15bbc3f61138

---
+++ backend/src/main/java/com/sss/tally/api/health/controller/HealthCheckController.java
@@ -0,0 +1,28 @@
+package com.sss.tally.api.health.controller;
+
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sss.tally.api.health.dto.HealthCheckResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthCheckController {
+	private final Environment environment;
+
+	@GetMapping("/health")
+	public ResponseEntity<HealthCheckResponseDto> healthCheck() {
+		HealthCheckResponseDto healthCheckResponseDto = HealthCheckResponseDto.builder()
+			.health("ok")
+			.activeProfiles(Arrays.asList(environment.getActiveProfiles()))
+			.build();
+		return ResponseEntity.status(HttpStatus.OK).body(healthCheckResponseDto);
+	}
+}
