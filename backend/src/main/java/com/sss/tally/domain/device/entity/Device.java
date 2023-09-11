d7e28e8b5a13e8377bc83ecac276e873270e34a7
박현철 <wns882@naver.com>
2023-09-11 16:54:29 +0900

    :sparkles: feat: Device Entity 생성
    Former-commit-id: 8ac5360cad286e2bc21354fb46a05868b1fa0be7

---
+++ backend/src/main/java/com/sss/tally/domain/device/entity/Device.java
@@ -0,0 +1,50 @@
+package com.sss.tally.domain.device.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.sss.tally.domain.member.entity.Member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Builder
+public class Device {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long deviceId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member memberId;
+
+	@Column(nullable = false)
+	private String deviceToken;
+
+	@Column(nullable = false)
+	private Boolean deviceStatus;
+
+	@CreatedDate
+	private LocalDateTime createDate;
+	
+}
