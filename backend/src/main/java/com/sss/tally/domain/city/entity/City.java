3245ee9034728c0c738cce554bb8650c2e131e23
박현철 <wns882@naver.com>
2023-09-11 16:53:50 +0900

    :sparkles: feat: City Entity 생성
    Former-commit-id: 1b5d9a4b38dc788faac8ac7817829e70dcf9b65d

---
+++ backend/src/main/java/com/sss/tally/domain/city/entity/City.java
@@ -0,0 +1,41 @@
+package com.sss.tally.domain.city.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.sss.tally.domain.state.entity.State;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Builder
+public class City {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cityId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "state_id")
+	private State stateId;
+
+	@Column(nullable = false)
+	private String cityName;
+
+}
