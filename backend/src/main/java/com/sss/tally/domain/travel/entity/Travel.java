3f646442f704ad039109d39de59b575d072a3b05
박현철 <wns882@naver.com>
2023-09-11 16:55:50 +0900

    :sparkles: feat: Travel Entity 생성
    Former-commit-id: 579aa5799f790539d05ac54e1809dcc4c62577e0

---
+++ backend/src/main/java/com/sss/tally/domain/travel/entity/Travel.java
@@ -0,0 +1,57 @@
+package com.sss.tally.domain.travel.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Travel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long travelId;
+
+	@Column(nullable = false)
+	private String travelTitle;
+
+	@Column(nullable = false)
+	private String travelImage;
+
+	@Column(nullable = false)
+	private LocalDateTime startDate;
+
+	@Column(nullable = false)
+	private LocalDateTime endDate;
+
+	@Column(nullable = false)
+	private Boolean status;
+
+	@CreatedDate
+	@Column(nullable = false)
+	private LocalDateTime createDate;
+
+	@Column(nullable = false)
+	private long travelLocation;
+
+	@Column(nullable = false)
+	private TravelTypeEnum travelType;
+
+}
