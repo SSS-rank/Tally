d889cb3cd4b2dd8e19620bf0aa72c406fea1a701
jungeunevepark <evecomcom98@naver.com>
2023-09-12 11:19:57 +0900

    :zap: feat: WebConfig에 XSS 방어 설정
    Former-commit-id: 3f335d2230d6241cfd1ae4264c28dca316c4920b

--- backend/src/main/java/com/sss/tally/global/web/WebConfig.java
+++ backend/src/main/java/com/sss/tally/global/web/WebConfig.java
@@ -1,12 +1,23 @@
 package com.sss.tally.global.web;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+	private final ObjectMapper objectMapper;
 	public void addCorsMappings(CorsRegistry registry){
 		registry.addMapping("/**")
 			.allowedOrigins(
@@ -20,4 +31,17 @@ public class WebConfig implements WebMvcConfigurer {
 				HttpMethod.OPTIONS.name()
 			);
 	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
+		converters.add(jsonEscapeConverter());
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jsonEscapeConverter(){
+		ObjectMapper copy = objectMapper.copy();
+		copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+		return new MappingJackson2HttpMessageConverter(copy);
+	}
+
 }
