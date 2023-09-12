e7c34086709fe7712f832b958867336a23f2139a
rladbwls30 <rladbwls30@naver.com>
2023-09-12 10:32:14 +0900

    :art: refactor: FeignClient 사용하는 코드로 수정
    Former-commit-id: 0b26abc2a5517657b67281c09e7a9762e48364d4

--- src/main/java/com/sss/bank/api/country/controller/CountryController.java
+++ src/main/java/com/sss/bank/api/country/controller/CountryController.java
@@ -20,7 +20,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sss.bank.api.country.client.CountryClient;
 import com.sss.bank.api.country.dto.CountryDto;
+import com.sss.bank.api.country.dto.CountryInfoDto;
+import com.sss.bank.api.kakaotoken.dto.KakaoTokenDto;
 import com.sss.bank.domain.country.entity.Country;
 import com.sss.bank.domain.country.repository.CountryRepository;
 
@@ -29,31 +32,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/country")
 public class CountryController {
+	private final CountryClient countryClient;
 	private final CountryRepository countryRepository;
+
 	@GetMapping
-	public ResponseEntity<List<CountryDto.Response>> getCountryInfo() throws Exception {
-		String result = "";
-		List<CountryDto.Response> countryResponse = new ArrayList<>();
-		URL url = new URL("https://api.odcloud.kr/api/15091117/v1/uddi:f340c326-d04c-43ab-bd1a-a3caf2c273e3?page=1&perPage=196&serviceKey=lQexMZMXDyFSmMCHE%2BbQj%2Fcm7uDGA%2Fn0AuNXPx0rfTPz%2BDOj%2FRX8U1Ay50ntvW2CJgOgOpMvoBYNohwsp%2BQMsw%3D%3D");
-		HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-		urlConnection.setRequestMethod("GET");
-		urlConnection.setRequestProperty("Content-type", "application/json");
+	public ResponseEntity<List<Country>> getCountry() {
+		if(countryRepository.count()==0) {
+			String serviceKey = "lQexMZMXDyFSmMCHE%2BbQj%2Fcm7uDGA%2Fn0AuNXPx0rfTPz%2BDOj%2FRX8U1Ay50ntvW2CJgOgOpMvoBYNohwsp%2BQMsw%3D%3D";
+			CountryDto.CountryReq countryReq = CountryDto.CountryReq.of(serviceKey,1,196);
+			CountryDto.CountryResp countryResp = countryClient.requestCountryInfo(countryReq);
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
-		result = br.readLine();
-
-		JSONParser jsonParser = new JSONParser();
-		JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
-		JSONArray jsonArray = (JSONArray) jsonObject.get("data");
-		for(int i=0;i<jsonArray.size();i++){
-			JSONObject object = (JSONObject) jsonArray.get(i);
-			String countryCode = (String)object.get("ISO alpha3");
-			String countryName = (String)object.get("한글명");
-			Country country = Country.of(countryCode, countryName);
-			countryRepository.save(country);
-			countryResponse.add(CountryDto.Response.from(country));
+			for (CountryInfoDto countryInfo : countryResp.getData()) {
+				Country country = Country.of(countryInfo.getCountryCode(), countryInfo.getCountryName());
+				countryRepository.save(country);
+			}
 		}
 
-		return ResponseEntity.status(HttpStatus.OK).body(countryResponse);
+		return ResponseEntity.status(HttpStatus.OK).body(countryRepository.findAll());
 	}
 }
