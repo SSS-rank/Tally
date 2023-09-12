a277dd552568dbb7d7e01ebafd0647960df7d88b
rladbwls30 <rladbwls30@naver.com>
2023-09-12 13:50:30 +0900

    :art: refactor: Controller 내용 Service 로 이동하고 Controller 에서 서비스 호출
    Former-commit-id: b146fa2d97bf77be5df404cf2aa9279005116f7f

---
+++ src/main/java/com/sss/bank/domain/country/service/CountryService.java
@@ -0,0 +1,9 @@
+package com.sss.bank.domain.country.service;
+
+import java.util.List;
+
+import com.sss.bank.domain.country.entity.Country;
+
+public interface CountryService {
+	List<Country> getAllCountryInfo();
+}
---
+++ src/main/java/com/sss/bank/domain/country/service/CountryServiceImpl.java
@@ -0,0 +1,41 @@
+package com.sss.bank.domain.country.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sss.bank.api.country.client.CountryClient;
+import com.sss.bank.api.country.dto.CountryDto;
+import com.sss.bank.api.country.dto.CountryInfoDto;
+import com.sss.bank.domain.country.entity.Country;
+import com.sss.bank.domain.country.repository.CountryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CountryServiceImpl implements CountryService {
+	private final CountryRepository countryRepository;
+	private final CountryClient countryClient;
+
+	@Value("${country.service.key}")
+	private String serviceKey;
+
+	@Override
+	public List<Country> getAllCountryInfo() {
+		if (countryRepository.count() == 0) {
+			CountryDto.CountryReq countryReq = CountryDto.CountryReq.of(serviceKey, 1, 196);
+			CountryDto.CountryResp countryResp = countryClient.requestCountryInfo(countryReq);
+
+			for (CountryInfoDto.CountryInfoReqDto countryInfo : countryResp.getData()) {
+				Country country = Country.of(countryInfo.getCountryCode(), countryInfo.getCountryName());
+				countryRepository.save(country);
+			}
+		}
+
+		return countryRepository.findAll();
+	}
+}
