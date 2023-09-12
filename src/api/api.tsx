a21059e5fb4417e60641962bb6d39ec3a4765f2d
이다곤 <leedagon624@gmail.com>
2023-09-12 12:32:08 +0900

    :pencil2: fix: api interceptor 추가

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,8 +4,15 @@ const api = axios.create({
 	baseURL: `${process.env.REACT_APP_BACK_ADDR}`,
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+		// Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
 	},
+});
+api.interceptors.request.use((config) => {
+	const access_token = sessionStorage.getItem('accessToken');
+	if (access_token) {
+		config.headers.Authorization = `Bearer ${access_token}`;
+	}
+	return config;
 });
 
 export default api;
