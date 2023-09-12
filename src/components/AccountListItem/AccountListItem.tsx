3b63713c1fe6cde1a44a853c7739c1b0a1654617
이다곤 <leedagon624@gmail.com>
2023-09-12 16:06:05 +0900

    :zap: feat: 삭제 후 페이지 새로고침 추가

--- src/components/AccountListItem/AccountListItem.tsx
+++ src/components/AccountListItem/AccountListItem.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import api from '../../api/api';
-import DeleteAccountModal from '../Modal/DeleteAccountModal';
 interface accountListItemProps {
 	balance: number;
 	bankcode: string;
@@ -21,6 +23,7 @@ function AccountListItem({ balance, bankcode, accountNum }: accountListItemProps
 
 	const accountNumRef = useRef<HTMLDivElement | null>(null);
 	const [modalOpen, setModalOpen] = useState(false);
+	const [password, setPassword] = useState('');
 	const handleAccountClick = () => {
 		const accountNumElement = accountNumRef.current;
 
@@ -31,6 +34,9 @@ function AccountListItem({ balance, bankcode, accountNum }: accountListItemProps
 			navigate(`/accountdetail/${accountNumValue}`);
 		}
 	};
+	const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value);
+	};
 	const handleDeleteAccount = () => {
 		setModalOpen(true);
 	};
@@ -39,7 +45,7 @@ function AccountListItem({ balance, bankcode, accountNum }: accountListItemProps
 		setModalOpen(false);
 	};
 
-	const handleDeleteConfirm = async (password: string) => {
+	const handleDeleteConfirm = async () => {
 		try {
 			const accountDeleteReqDto = {
 				bank_code: { bankcode }.bankcode,
@@ -51,7 +57,7 @@ function AccountListItem({ balance, bankcode, accountNum }: accountListItemProps
 
 			if (response.status === 200) {
 				console.log('계좌 삭제 성공!');
-				navigate('/main');
+				window.location.replace('/main');
 			} else {
 				console.error('계정 삭제 실패:', response.data);
 			}
@@ -80,11 +86,45 @@ function AccountListItem({ balance, bankcode, accountNum }: accountListItemProps
 					</Button>
 				</CardActions>
 			</Card>
-			<DeleteAccountModal
+			<Modal
 				open={modalOpen}
-				handleClose={handleCloseModal}
-				handleDelete={handleDeleteConfirm}
-			/>
+				onClose={handleCloseModal}
+				aria-labelledby="modal-title"
+				aria-describedby="modal-description"
+			>
+				<Box
+					sx={{
+						position: 'absolute',
+						top: '50%',
+						left: '50%',
+						transform: 'translate(-50%, -50%)',
+						bgcolor: 'background.paper',
+						boxShadow: 24,
+						p: 4,
+						maxWidth: 400,
+						width: '90%',
+						textAlign: 'center',
+					}}
+				>
+					<h2 id="modal-title">계좌 삭제</h2>
+					<p id="modal-description">정말로 계좌를 삭제하시겠습니까?</p>
+					<TextField
+						label="비밀번호"
+						type="password"
+						variant="outlined"
+						fullWidth
+						value={password}
+						onChange={handleChangePassword}
+						sx={{ mb: 2 }}
+					/>
+					<Button variant="contained" onClick={handleDeleteConfirm}>
+						삭제
+					</Button>
+					<Button variant="contained" onClick={handleCloseModal} sx={{ ml: 2 }}>
+						취소
+					</Button>
+				</Box>
+			</Modal>
 		</Grid>
 	);
 }
