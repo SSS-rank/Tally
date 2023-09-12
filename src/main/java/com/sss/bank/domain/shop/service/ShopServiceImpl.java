9a8a3e3e74fe7294b7aa3c6ffa60f9cc74910796
jungeunevepark <evecomcom98@naver.com>
2023-09-12 12:56:47 +0900

    :zap: feat: 받은 나라 코드로 나라를 조회하는 로직 추가
    Former-commit-id: eb5bf7b9af2ae9e25e4cf642fd107bf59a17b681

--- src/main/java/com/sss/bank/domain/shop/service/ShopServiceImpl.java
+++ src/main/java/com/sss/bank/domain/shop/service/ShopServiceImpl.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sss.bank.api.shop.dto.ShopDto;
+import com.sss.bank.domain.country.entity.Country;
+import com.sss.bank.domain.country.repository.CountryRepository;
 import com.sss.bank.domain.shop.entity.Shop;
 import com.sss.bank.domain.shop.repository.ShopRepository;
 import com.sss.bank.global.error.ErrorCode;
+import com.sss.bank.global.error.exception.CountryException;
 import com.sss.bank.global.error.exception.ShopException;
 
 import lombok.RequiredArgsConstructor;
@@ -20,12 +23,18 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class ShopServiceImpl implements ShopService {
 	private final ShopRepository shopRepository;
+	private final CountryRepository countryRepository;
 
 	@Override
 	public ShopDto.ShopRespDto createShop(ShopDto.ShopReqDto shopReqDto) {
-		Shop shop = Shop.from(shopReqDto);
-		shopRepository.save(shop);
-		return ShopDto.ShopRespDto.from(shop);
+		Optional<Country> country = countryRepository.findCountryByCountryCode(shopReqDto.getShopNationCode());
+		if (country.isEmpty())
+			throw new CountryException(ErrorCode.NOT_EXIST_COUNTRY);
+
+		System.out.println(country.get());
+		Shop shop = Shop.of(shopReqDto, country.get());
+		Shop save = shopRepository.save(shop);
+		return ShopDto.ShopRespDto.from(save);
 	}
 
 	@Override
