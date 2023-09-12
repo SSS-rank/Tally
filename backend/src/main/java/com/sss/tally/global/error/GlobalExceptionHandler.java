588924f91b2e98b566a65578e22e003e5b440895
jungeunevepark <evecomcom98@naver.com>
2023-09-12 10:09:43 +0900

    :zap: feat: 지원하지 않는 HTTP method를 호출할 경우 client로 에러 메세지를 전달하는 함수 구현
    Former-commit-id: 669971478fa6530f066062eb9d5476e74a6243a4

--- backend/src/main/java/com/sss/tally/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/sss/tally/global/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sss.tally.global.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -27,6 +28,13 @@ public class GlobalExceptionHandler {
 		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 			.body(errorResponse);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected  ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+		log.error("handleHttpRequestMethodNotSupportedException", e);
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED.toString(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
 	}
 
 	@ExceptionHandler(value = {BusinessException.class})
