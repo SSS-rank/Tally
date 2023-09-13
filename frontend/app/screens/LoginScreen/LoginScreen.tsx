57bec759739b86fd78b3e1673b770e74c1141a9e
kosigi19 <kosigi19@naver.com>
2023-09-13 16:36:27 +0900

    ➕ add: UI 라이브러리 추가 및 적용
    Former-commit-id: 92dff8557cc093a5abbc45f1e3e09ff3cf6c1ad0

--- frontend/app/screens/LoginScreen/LoginScreen.tsx
+++ frontend/app/screens/LoginScreen/LoginScreen.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackProps} from '../../navigation/RootStack';
+import {useTheme, Badge} from 'react-native-paper';
 
 type RootStackProp = NativeStackScreenProps<RootStackProps, 'SignIn'>;
 
 function LoginScreen({route}: RootStackProp) {
+  const theme = useTheme();
   const {setUserToken} = route.params;
 
   return (
     <View style={styles.viewContainer}>
-      <Text>Tally</Text>
+      <Text style={{backgroundColor: theme.colors.primary}}>Tally</Text>
       <Button title="Sign Up" onPress={() => setUserToken('token')} />
+      <Badge>3</Badge>
     </View>
   );
 }
