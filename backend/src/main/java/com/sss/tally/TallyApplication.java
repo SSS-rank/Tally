fc1ff79906a394e9a396d3e0cd3b053c20dbb9ba
rladbwls30 <rladbwls30@naver.com>
2023-09-13 16:54:59 +0900

    :zap: feat: FeignClient 사용 위한 Annotation 추가
    Former-commit-id: 379b032604c95c8fc8e436febb41ab69c102fdda

--- backend/src/main/java/com/sss/tally/TallyApplication.java
+++ backend/src/main/java/com/sss/tally/TallyApplication.java
@@ -2,8 +2,10 @@ package com.sss.tally;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TallyApplication {
 
 	public static void main(String[] args) {
