2d6cf097e0ed0041292b9817bdc0c3f3fcef362b
rladbwls30 <rladbwls30@naver.com>
2023-09-13 17:06:34 +0900

    :zap: feat: kakao에서 가져올 회원 정보 DTO 생성
    Former-commit-id: 68794e358d1cc63c5d039fceae843ecaba5ebdaa

---
+++ backend/src/main/java/com/sss/tally/external/kakao/dto/KakaoMemberInfoRespDto.java
@@ -0,0 +1,28 @@
+package com.sss.tally.external.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoMemberInfoRespDto {
+	private Long id;
+
+	@JsonProperty("kakao_account")
+	private KakaoAccount kakaoAccount;
+
+	@Getter
+	@Setter
+	public static class KakaoAccount {
+		private Profile profile;
+
+		@Getter
+		@Setter
+		public static class Profile {
+			private String nickname;
+			private String profileImageUrl;
+		}
+	}
+}
