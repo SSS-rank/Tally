73b8c094339ae3238c73f65bb8e6d3ac08144bc4
rladbwls30 <rladbwls30@naver.com>
2023-09-14 13:22:50 +0900

    :sparkles: feat: Device 조회 위한 repository 생성
    Former-commit-id: 7a409d3fe0c65125bcacf4153f86522777b09be1

---
+++ backend/src/main/java/com/sss/tally/domain/device/repository/DeviceRepository.java
@@ -0,0 +1,8 @@
+package com.sss.tally.domain.device.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sss.tally.domain.device.entity.Device;
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+}
