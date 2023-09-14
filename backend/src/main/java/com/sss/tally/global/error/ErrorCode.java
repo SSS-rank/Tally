15495e1b3b624925f4eb02ec4a04292afcb67f00
박현철 <wns882@naver.com>
2023-09-14 15:45:34 +0900

    :zap: feat: 디바이스 토큰 유효성 검사 및 알림 전송실패 에러 코드 추가
    Former-commit-id: c6b509571a54d49a1b7aa7d10b26c6766714b669

--- backend/src/main/java/com/sss/tally/global/error/ErrorCode.java
+++ backend/src/main/java/com/sss/tally/global/error/ErrorCode.java
@@ -6,12 +6,16 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	;
-	ErrorCode(HttpStatus httpStatus, String errorCode, String message){
+	//알림
+	FAIL_SEND_NOTIFICATION(HttpStatus.BAD_REQUEST, "001", "알림 전송이 실패했습니다."),
+	NOT_VALID_DEVICETOKEN(HttpStatus.BAD_REQUEST, "001", "유효하지 않은 디바이스토큰입니다.");
+
+	ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
 		this.httpStatus = httpStatus;
 		this.errorCode = errorCode;
 		this.message = message;
 	}
+
 	private HttpStatus httpStatus;
 	private String errorCode;
 	private String message;
