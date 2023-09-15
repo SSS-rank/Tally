14ea4cebc9c3a993ed2df7bf10134252c6d44548
박현철 <wns882@naver.com>
2023-09-15 16:47:32 +0900

    :zap: feat: 정산요청하기 Dto 추가
    Former-commit-id: 9fb106fe220651fce5577b016f87d398cd48872b

---
+++ backend/src/main/java/com/sss/tally/api/calculate/dto/CalculateDto.java
@@ -0,0 +1,20 @@
+package com.sss.tally.api.calculate.dto;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class CalculateDto {
+
+	@NoArgsConstructor
+	@Getter
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class CalculateCreateDto {
+		@NotNull
+		private String paymentUuid;
+	}
+}
