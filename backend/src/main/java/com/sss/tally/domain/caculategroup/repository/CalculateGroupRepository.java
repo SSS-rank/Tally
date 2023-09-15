1308ba44fdb262663f96d7bc75fb3d39ecbce139
박현철 <wns882@naver.com>
2023-09-15 16:48:11 +0900

    :zap: feat: CalculateGroup repository 추가
    Former-commit-id: ac98c383bd2f249fe268c8c8800cbe47f973658d

---
+++ backend/src/main/java/com/sss/tally/domain/caculategroup/repository/CalculateGroupRepository.java
@@ -0,0 +1,9 @@
+package com.sss.tally.domain.caculategroup.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sss.tally.domain.caculategroup.entity.CalculateGroup;
+
+public interface CalculateGroupRepository extends JpaRepository<CalculateGroup, Integer> {
+
+}
