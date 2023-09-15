3e3b41bd24a7af3f2ffb7647e7130ddc136a9669
zero9657@naver.com <zero9657@naver.com>
2023-09-15 16:19:54 +0900

    ✨ feat :  카카오 로그인 개발
    Former-commit-id: 411069c43489c72194c6dad06954a26e56e966f6

--- frontend/app/screens/LoginScreen/LoginScreen.tsx
+++ frontend/app/screens/LoginScreen/LoginScreen.tsx
@@ -4,19 +4,40 @@ import { Button } from 'react-native-paper';
 
 import * as KakaoLogin from '@react-native-seoul/kakao-login';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useSetRecoilState } from 'recoil';
 
+import api from '../../api/api';
 import { RootStackProps } from '../../navigation/RootStack';
+import { TokenState } from '../../recoil/recoil';
 
 type RootStackProp = NativeStackScreenProps<RootStackProps, 'SignIn'>;
 
 function LoginScreen({ route }: RootStackProp) {
 	const { setUserToken } = route.params;
+	const setTokenState = useSetRecoilState(TokenState);
 
 	const login = async () => {
 		KakaoLogin.login()
-			.then((result) => {
-				console.log('login success', JSON.stringify(result));
+			.then(async (result) => {
+				// console.log('login success', JSON.stringify(result));
+				const accessToken = result.accessToken;
 				getProfile();
+
+				try {
+					const data = {
+						kakaoAccessToken: accessToken,
+						deviceToken: `ci0kdlHsTxirohKvceSNrn:APA91bGSSysqDgEsg1CFmYB9xw0B4h7jINzWhMxem5S1__93oC9co1bitM1Kz9z7zM7FNafBVXr_rc7i-vBavYLeyM29hTJpec3B1dHcWyUo44JIxnfLDFINHXd7-ap6i4Uk1Jnyausc`,
+					};
+					const res = await api.post(`/login`, data);
+
+					if (res.status === 200) {
+						console.log(res.data);
+						setTokenState(res.data);
+						setUserToken(res.data);
+					}
+				} catch (error) {
+					console.error(error);
+				}
 			})
 			.catch((error) => {
 				if (error.code === 'E_CANCELLED_OPERATION') {
@@ -30,7 +51,7 @@ function LoginScreen({ route }: RootStackProp) {
 	const getProfile = () => {
 		KakaoLogin.getProfile()
 			.then((result) => {
-				console.log(`getProfile Success`, JSON.stringify(result));
+				// console.log(`getProfile Success`, JSON.stringify(result));
 			})
 			.catch((err) => {
 				console.log(`getProfile fail ${err.code} ${err.message}`);
