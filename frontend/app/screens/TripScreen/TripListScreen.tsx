c8a23d53c316352bb1a2c0ab9fb81a693d588c12
zero9657@naver.com <zero9657@naver.com>
2023-09-15 11:09:13 +0900

    ✨ feat : 여행지 생성 버튼 추가
    Former-commit-id: 046f07eabe18de91ec41f4ead922a9f6da235052

--- frontend/app/screens/TripScreen/TripListScreen.tsx
+++ frontend/app/screens/TripScreen/TripListScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Text, View, Button, StyleSheet, TextInput } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { TripStackProps } from '../../navigation/TripStack';
 
@@ -12,34 +13,48 @@ function TripListScreen({ navigation }: TripStackProp) {
 	const [searchText, setSearchText] = useState('');
 
 	return (
-		<>
-			<View style={styles.viewContainer}>
-				<View>
-					<Text style={styles.titleText}>나의 여행지</Text>
-					<View style={styles.searchView}>
-						<TextInput
-							style={styles.searchBar}
-							placeholder="검색어를 입력해주세요"
-							onChangeText={setSearchText}
-							value={searchText}
-						/>
-						<Icon name="search" style={styles.searchIcon} />
+		<View style={styles.viewContainer}>
+			<View>
+				<Text style={styles.titleText}>나의 여행지</Text>
+				<View style={styles.searchView}>
+					<TextInput
+						style={styles.searchBar}
+						placeholder="검색어를 입력해주세요"
+						onChangeText={setSearchText}
+						value={searchText}
+					/>
+					<Icon name="search" style={styles.searchIcon} />
+				</View>
+			</View>
+			<TouchableOpacity style={styles.tripAddBtn}>
+				<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+					<MaterialIcon
+						name="plus-circle"
+						size={48}
+						style={{ color: '#91C0EB', marginRight: 10 }}
+					/>
+					<View>
+						<Text style={{ fontSize: 20, fontWeight: '500', color: '#232323' }}>여행 만들기</Text>
+						<Text style={{ fontSize: 16, fontWeight: '500', color: '#666666' }}>
+							여행을 등록하고 떠나보세요
+						</Text>
 					</View>
 				</View>
-				<View>{/* TODO : 여행지 생성 버튼 */}</View>
-				<View>{/* TODO : 다가오는 여행 */}</View>
-				<View>{/* TODO : ㅇ행 중 */}</View>
-				<View>{/* TODO : 여행지 생성 버튼 */}</View>
-				<Button title="여행 추가" onPress={() => navigation.navigate('CreateTrip')} />
-				<Button title="상세" onPress={() => navigation.navigate('TripDetail')} />
-			</View>
-		</>
+			</TouchableOpacity>
+			<View>{/* TODO : 다가오는 여행 */}</View>
+			<View>{/* TODO : ㅇ행 중 */}</View>
+			<View>{/* TODO : 여행지 생성 버튼 */}</View>
+			{/* <Button title="여행 추가" onPress={() => navigation.navigate('CreateTrip')} />
+				<Button title="상세" onPress={() => navigation.navigate('TripDetail')} /> */}
+		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	viewContainer: {
-		margin: 15,
+		flex: 1,
+		padding: 15,
+		backgroundColor: '#ffffff',
 	},
 	titleText: { fontSize: 30, fontWeight: 'bold', marginBottom: 30, color: '#232323' },
 	searchView: {
@@ -60,6 +75,13 @@ const styles = StyleSheet.create({
 		position: 'absolute',
 		bottom: 55,
 		right: 10,
+	},
+	tripAddBtn: {
+		backgroundColor: '#F6F6F6',
+		color: '#232323',
+		borderRadius: 8,
+		paddingVertical: 15,
+		paddingHorizontal: 20,
 	},
 });
 
