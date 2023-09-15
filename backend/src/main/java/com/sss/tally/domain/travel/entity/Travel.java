f60c20dc8a517010214478f23cc3d68293948a16
박현철 <wns882@naver.com>
2023-09-15 16:52:59 +0900

    :bug: fix: Enum 타입 string으로 변경
    Former-commit-id: 2828fc841ec2c5c7eee3d2a476f31fa1b01d6141

--- backend/src/main/java/com/sss/tally/domain/travel/entity/Travel.java
+++ backend/src/main/java/com/sss/tally/domain/travel/entity/Travel.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -51,6 +53,7 @@ public class Travel {
 	@Column(nullable = false)
 	private long travelLocation;
 
+	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private TravelTypeEnum travelType;
 
