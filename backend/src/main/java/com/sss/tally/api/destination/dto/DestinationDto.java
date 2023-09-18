25b6f1df45d05fe76ce9ad5de40929eddd939924
jungeunevepark <evecomcom98@naver.com>
2023-09-18 17:56:44 +0900

    :zap: feat: Feign Client에 사용되는 응답, 반환 객체 생성
    Former-commit-id: 68dd3f489fbd922c74ea49e2a1d0f3ecc97b1e72

--- backend/src/main/java/com/sss/tally/api/destination/dto/DestinationDto.java
+++ backend/src/main/java/com/sss/tally/api/destination/dto/DestinationDto.java
@@ -1,24 +1,60 @@
 package com.sss.tally.api.destination.dto;
 
+import java.util.List;
+
+import org.springframework.data.repository.cdi.Eager;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.sss.tally.domain.city.entity.City;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 public class DestinationDto {
+
 	@Getter
 	@Builder
-	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-	public static class CityDto{
-		private String cityName;
-		public static CityDto from(City city){
-			return CityDto.builder()
-				.cityName(city.getCityName())
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class CityReqDto{
+		private String code;
+		private String name;
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class StatusRespListDto{
+		private List<StatusCityRespDto> regcodes;
+	}
+
+	@Builder
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class StatusCityReqDto{
+		private String regcode_pattern;
+		private final boolean is_ignore_zero = true;
+
+		public static StatusCityReqDto from(String regcodePattern){
+			return StatusCityReqDto.builder()
+				.regcode_pattern(regcodePattern)
 				.build();
 		}
+	}
+
+	@Getter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class StatusCityRespDto{
+		private String code;
+		private String name;
 	}
 }
