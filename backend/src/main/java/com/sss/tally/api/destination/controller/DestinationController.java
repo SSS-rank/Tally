02ed15a24dc3465bd6df6efe95854ce59f82f1a8
jungeunevepark <evecomcom98@naver.com>
2023-09-18 17:58:03 +0900

    :zap: feat: 시도 정보 조회 API 생성
    Former-commit-id: 572278a69c0b08d5079fec79255ed25e6ce8979b

--- backend/src/main/java/com/sss/tally/api/destination/controller/DestinationController.java
+++ backend/src/main/java/com/sss/tally/api/destination/controller/DestinationController.java
@@ -1,11 +1,17 @@
 package com.sss.tally.api.destination.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sss.tally.api.destination.client.StatusClient;
 import com.sss.tally.api.destination.dto.DestinationDto;
 import com.sss.tally.api.destination.service.DestinationService;
 
@@ -16,9 +22,22 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/destination")
 public class DestinationController {
 	private final DestinationService destinationService;
-	@GetMapping("/city")
-	public ResponseEntity<DestinationDto.CityDto> getCityList(@RequestParam int state){
-		destinationService.getCityList(state);
-		return null;
+	private final StatusClient statusClient;
+
+	@PostMapping("/status")
+	public ResponseEntity<List<DestinationDto.StatusCityRespDto>> getStatusList(){
+		String contentType = "application/json";
+		String statusCode = "*00000000";
+		DestinationDto.StatusCityReqDto statusCityReqDto = DestinationDto.StatusCityReqDto.from(statusCode);
+		DestinationDto.StatusRespListDto statusRespDtoList = statusClient.requestStatusInfo(contentType, statusCityReqDto);
+		return ResponseEntity.status(HttpStatus.OK).body(statusRespDtoList.getRegcodes());
+	}
+
+	@PostMapping("/city/{code}")
+	public ResponseEntity<List<DestinationDto.StatusCityRespDto>> getCityList(@PathVariable String code){
+		String contentType = "application/json";
+		DestinationDto.StatusCityReqDto statusCityReqDto = DestinationDto.StatusCityReqDto.from(code);
+		DestinationDto.StatusRespListDto statusRespDtoList = statusClient.requestStatusInfo(contentType, statusCityReqDto);
+		return ResponseEntity.status(HttpStatus.OK).body(statusRespDtoList.getRegcodes());
 	}
 }
