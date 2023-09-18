31f2d799051b8baa4452e6c07358e6a4aaf80ef5
zero9657@naver.com <zero9657@naver.com>
2023-09-18 12:25:39 +0900

    ✨ feat : filter 선택 모달 ui 개발
    Former-commit-id: adc22e335eabf0ca76fa3135b2738e57539ccbb0

--- frontend/app/screens/TripScreen/TripListScreen.tsx
+++ frontend/app/screens/TripScreen/TripListScreen.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Button, Portal, Modal } from 'react-native-paper';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/Ionicons';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import TripListFilter from '../../components/TripListFilter/TripListFilter';
 import { TripStackProps } from '../../navigation/TripStack';
 
 type TripStackProp = NativeStackScreenProps<TripStackProps, 'TripList'>;
 
 function TripListScreen({ navigation }: TripStackProp) {
 	const [searchText, setSearchText] = useState('');
+	const [modalVisible, setModalVisible] = useState(false);
+
+	const openFilter = () => {
+		setModalVisible(true);
+	};
+
+	const hideModal = () => {
+		setModalVisible(false);
+	};
 
 	return (
 		<View style={styles.viewContainer}>
@@ -34,18 +45,48 @@ function TripListScreen({ navigation }: TripStackProp) {
 						style={{ color: '#91C0EB', marginRight: 10 }}
 					/>
 					<View>
-						<Text style={{ fontSize: 20, fontWeight: '500', color: '#232323' }}>여행 만들기</Text>
-						<Text style={{ fontSize: 16, fontWeight: '500', color: '#666666' }}>
+						<Text style={{ fontSize: 18, fontWeight: '500', color: '#232323' }}>여행 만들기</Text>
+						<Text style={{ fontSize: 14, fontWeight: '500', color: '#666666' }}>
 							여행을 등록하고 떠나보세요
 						</Text>
 					</View>
 				</View>
 			</TouchableOpacity>
+			<Button
+				icon="chevron-down"
+				mode="text"
+				onPress={openFilter}
+				textColor="#666666"
+				style={{ alignItems: 'flex-start' }}
+				contentStyle={{ flexDirection: 'row-reverse' }}
+				labelStyle={{ fontSize: 18, textAlign: 'left' }}
+			>
+				필터
+			</Button>
 			<View>{/* TODO : 다가오는 여행 */}</View>
 			<View>{/* TODO : ㅇ행 중 */}</View>
 			<View>{/* TODO : 여행지 생성 버튼 */}</View>
 			{/* <Button title="여행 추가" onPress={() => navigation.navigate('CreateTrip')} />
 				<Button title="상세" onPress={() => navigation.navigate('TripDetail')} /> */}
+
+			<Portal>
+				<Modal
+					visible={modalVisible}
+					onDismiss={hideModal}
+					contentContainerStyle={styles.modalContainer}
+				>
+					<View style={styles.modalView}>
+						<Text
+							style={{ fontSize: 20, fontWeight: 'bold', marginVertical: 16, color: '#232323' }}
+						>
+							필터 선택
+						</Text>
+						<TripListFilter filterName="다가오는 여행" />
+						<TripListFilter filterName="여행 중" />
+						<TripListFilter filterName="다가오는 여행" />
+					</View>
+				</Modal>
+			</Portal>
 		</View>
 	);
 }
@@ -82,6 +123,18 @@ const styles = StyleSheet.create({
 		borderRadius: 8,
 		paddingVertical: 15,
 		paddingHorizontal: 20,
+		marginBottom: 40,
+	},
+	modalContainer: {
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		width: '100%',
+	},
+	modalView: {
+		backgroundColor: '#fff',
+		borderRadius: 8,
+		padding: 20,
 	},
 });
 
