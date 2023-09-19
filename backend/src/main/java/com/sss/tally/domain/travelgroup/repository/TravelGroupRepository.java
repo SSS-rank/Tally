6b6f1ff745ea8519ba2f17a1c63862a03340f1e2
jungeunevepark <evecomcom98@naver.com>
2023-09-19 15:04:15 +0900

    :zap: feat: TravelGroupRepository 생성 및 여행을 생성하는 API 구현
    Former-commit-id: 84baa0386a8dd3a706d045d5df795b9c8cf8322a

---
+++ backend/src/main/java/com/sss/tally/domain/travelgroup/repository/TravelGroupRepository.java
@@ -0,0 +1,8 @@
+package com.sss.tally.domain.travelgroup.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sss.tally.domain.travelgroup.entity.TravelGroup;
+
+public interface TravelGroupRepository extends JpaRepository<TravelGroup, Long> {
+}
