0d79db502aee223c0b893c90f570a19cde393db6
박현철 <wns882@naver.com>
2023-09-19 13:31:03 +0900

    :zap: feat: 정산 요청 리스트 가져오는 DTO 추가
    Former-commit-id: d45dee4ed200dc9686b38a70f4db39d110b24f0c

--- backend/src/main/java/com/sss/tally/api/calculate/dto/CalculateDto.java
+++ backend/src/main/java/com/sss/tally/api/calculate/dto/CalculateDto.java
@@ -1,10 +1,16 @@
 package com.sss.tally.api.calculate.dto;
 
+import java.time.format.DateTimeFormatter;
+
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sss.tally.domain.caculategroup.entity.CalculateGroup;
+import com.sss.tally.domain.caculategroup.entity.CalculateGroupStatusEnum;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +22,33 @@ public class CalculateDto {
 	public static class CalculateCreateReqDto {
 		@NotNull
 		private String paymentUuid;
+	}
+
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	public static class GetRequestCalculateListRespDto {
+
+		private String calculateGroupUuid;
+
+		private Long amount;
+
+		private CalculateGroupStatusEnum status;
+
+		private String createdTime;
+
+		public static GetRequestCalculateListRespDto of(Long amount, CalculateGroup calculateGroup) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+			String formattedTime = calculateGroup.getCreateDate().format(formatter);
+			return GetRequestCalculateListRespDto.builder()
+				.amount(amount)
+				.calculateGroupUuid(calculateGroup.getCalculateGroupUuid())
+				.createdTime(formattedTime)
+				.status(calculateGroup.getStatus())
+				.build();
+		}
 	}
 
 }
