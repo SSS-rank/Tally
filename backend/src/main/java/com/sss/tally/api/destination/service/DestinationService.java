d61a32ba534a8724eba7a4e162df28147cc81478
jungeunevepark <evecomcom98@naver.com>
2023-09-19 11:52:07 +0900

    :zap: feat: 특별시, 광역시 등의 정보를 저장하는 함수 구현
    Former-commit-id: 8cd2474902428cd9560e7e609bd13193fd63fc51

--- backend/src/main/java/com/sss/tally/api/destination/service/DestinationService.java
+++ backend/src/main/java/com/sss/tally/api/destination/service/DestinationService.java
@@ -1,6 +1,9 @@
 package com.sss.tally.api.destination.service;
 
+import java.util.List;
+
 import com.sss.tally.api.destination.dto.DestinationDto;
 
 public interface DestinationService {
+	void createStatus(List<DestinationDto.StateCityRespDto> statusList);
 }
--- backend/src/main/java/com/sss/tally/api/destination/service/DestinationServiceImpl.java
+++ backend/src/main/java/com/sss/tally/api/destination/service/DestinationServiceImpl.java
@@ -1,9 +1,19 @@
 package com.sss.tally.api.destination.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sss.tally.api.destination.dto.DestinationDto;
+import com.sss.tally.domain.city.entity.City;
+import com.sss.tally.domain.city.repository.CityRepository;
+import com.sss.tally.domain.state.entity.State;
+import com.sss.tally.domain.state.repository.StateRepository;
+import com.sss.tally.global.error.ErrorCode;
+import com.sss.tally.global.error.exception.BusinessException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +21,13 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class DestinationServiceImpl implements DestinationService{
+	private final CityRepository cityRepository;
+	private final StateRepository stateRepository;
+
+	@Override
+	public void createStatus(List<DestinationDto.StateCityRespDto> stateList) {
+		for(DestinationDto.StateCityRespDto state: stateList)
+			stateRepository.save(State.from(state));
+	}
 
 }
