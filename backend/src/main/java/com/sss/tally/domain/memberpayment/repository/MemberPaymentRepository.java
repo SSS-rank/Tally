4081bdb1089509b2bce0f69ef9ee29292bef6063
박현철 <wns882@naver.com>
2023-09-19 14:52:38 +0900

    :zap: feat: MemberPayment 리스트를 멤버로 찾는 기능 추가
    Former-commit-id: b7adba769c5582fe4172a78541de1a1101fd229d

--- backend/src/main/java/com/sss/tally/domain/memberpayment/repository/MemberPaymentRepository.java
+++ backend/src/main/java/com/sss/tally/domain/memberpayment/repository/MemberPaymentRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.memberpayment.entity.MemberPayment;
 import com.sss.tally.domain.payment.entity.Payment;
 
 public interface MemberPaymentRepository extends JpaRepository<MemberPayment, Integer> {
 	List<MemberPayment> findMemberPaymentsByPaymentId(Payment payment);
+
+	List<MemberPayment> findMemberPaymentsByPaymentIdAndMemberId(Payment payment, Member member);
 }
