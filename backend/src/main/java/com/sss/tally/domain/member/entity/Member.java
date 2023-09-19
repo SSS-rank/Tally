7556ddd55e10c0d733df8d93a213e1bb315827e9
jungeunevepark <evecomcom98@naver.com>
2023-09-19 14:59:27 +0900

    :sparkles: feat: TravelCreateDto에서 Travel로 변환하는 정적 팩토리 메서드 생성
    Former-commit-id: 0c3cb6f28b059ce4d9d52cd4a0fefdecf689d3ba

--- backend/src/main/java/com/sss/tally/domain/member/entity/Member.java
+++ backend/src/main/java/com/sss/tally/domain/member/entity/Member.java
@@ -21,6 +21,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -28,6 +29,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
+@ToString
 public class Member implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
