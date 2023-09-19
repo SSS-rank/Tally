be65dfd33acf0a026da03247613a782dafa7ff7c
rladbwls30 <rladbwls30@naver.com>
2023-09-19 16:13:35 +0900

    :pencil2: fix: 회원탈퇴 post에서 patch로 변경
    Former-commit-id: d511ad75f7fd842307f3f0ad5595ea8d87793e83

--- backend/src/main/java/com/sss/tally/api/member/controller/MemberController.java
+++ backend/src/main/java/com/sss/tally/api/member/controller/MemberController.java
@@ -3,7 +3,7 @@ package com.sss.tally.api.member.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	private final MemberService memberService;
 
-	@PostMapping("/withdrawal")
+	@PatchMapping("/withdrawal")
 	public ResponseEntity<String> withdrawal(Authentication authentication){
 		memberService.withdrawal(authentication);
 		return ResponseEntity.status(HttpStatus.OK).body("Withdrawal Success");
