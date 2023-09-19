64be5c13d8a0bd95780d19e7b6e3ca865c927b8f
rladbwls30 <rladbwls30@naver.com>
2023-09-19 11:00:13 +0900

    :pencil2: fix: jwt token 생성시 member id 대신 member uuid 로 변경
    Former-commit-id: 4f2576e6f805c0183eb94722a2210c052e06b913

--- backend/src/main/java/com/sss/tally/global/jwt/service/JwtProvider.java
+++ backend/src/main/java/com/sss/tally/global/jwt/service/JwtProvider.java
@@ -35,44 +35,44 @@ public class JwtProvider {
 	private final MemberRepository memberRepository;
 
 	//현재 시간으로부터 30분 뒤의 시간을 리턴
-	public Date createAccessTokenExpireTime() {
-		return new Date(System.currentTimeMillis() + Long.parseLong(accessTokenExpirationTime));
+	public Date createAccessTokenExpireTime(){
+		return new Date(System.currentTimeMillis()+Long.parseLong(accessTokenExpirationTime));
 	}
 
 	//현재 시간으로부터 2주 뒤의 시간을 리턴
-	public Date createRefreshTokenExpireTime() {
-		return new Date(System.currentTimeMillis() + Long.parseLong(refreshTokenExpirationTime));
+	public Date createRefreshTokenExpireTime(){
+		return new Date(System.currentTimeMillis()+Long.parseLong(refreshTokenExpirationTime));
 	}
 
 	// AccessToken 생성
-	public String createAccessToken(Long memberId, Date expirationTime) {
+	public String createAccessToken(String memberUuid, Date expirationTime) {
 		return Jwts.builder()
 			.setSubject(TokenType.ACCESS.name())
 			.setIssuedAt(new Date())
 			.setExpiration(expirationTime)
-			.claim("memberId", memberId)
+			.claim("memberUuid", memberUuid)
 			.signWith(SignatureAlgorithm.HS512, tokenSecret.getBytes(StandardCharsets.UTF_8))
-			.setHeaderParam("typ", "JWT")
+			.setHeaderParam("typ","JWT")
 			.compact();
 	}
 
 	// RefreshToken 생성
-	public String createRefreshToken(Long memberId, Date expirationTime) {
+	public String createRefreshToken(String memberUuid, Date expirationTime){
 		return Jwts.builder()
 			.setSubject(TokenType.REFRESH.name())
 			.setIssuedAt(new Date())
 			.setExpiration(expirationTime)
-			.claim("memberId", memberId)
+			.claim("memberUuid", memberUuid)
 			.signWith(SignatureAlgorithm.HS512, tokenSecret.getBytes(StandardCharsets.UTF_8))
 			.compact();
 	}
 
 	// JwtToken 생성
-	public JwtTokenDto createJwtTokenDto(Long memberId) {
+	public JwtTokenDto createJwtTokenDto(String memberUuid){
 		Date accessTokenExpireTime = createAccessTokenExpireTime();
 		Date refreshTokenExpireTime = createRefreshTokenExpireTime();
-		String accessToken = createAccessToken(memberId, accessTokenExpireTime);
-		String refreshToken = createRefreshToken(memberId, refreshTokenExpireTime);
+		String accessToken = createAccessToken(memberUuid, accessTokenExpireTime);
+		String refreshToken = createRefreshToken(memberUuid, refreshTokenExpireTime);
 
 		return JwtTokenDto.builder()
 			.grantType(GrantType.BEARER.getType())
@@ -84,13 +84,13 @@ public class JwtProvider {
 	}
 
 	// 토큰 유효성 검사
-	public boolean validateToken(String token) {
-		try {
+	public boolean validateToken(String token){
+		try{
 			Jwts.parser().setSigningKey(tokenSecret.getBytes(StandardCharsets.UTF_8))
 				.parseClaimsJws(token);
 		} catch (ExpiredJwtException e) {
 			throw new AuthenticationException(ErrorCode.EXPIRED_TOKEN);
-		} catch (Exception e) {
+		} catch (Exception e){
 			throw new AuthenticationException(ErrorCode.NOT_VALID_TOKEN);
 		}
 		return true;
@@ -109,10 +109,10 @@ public class JwtProvider {
 	}
 
 	// JWT 로 인증정보 조회
-	public Authentication getAuthentication(String token) {
+	public Authentication getAuthentication(String token){
 		Claims claims = this.getTokenClaims(token);
-		Long memberId = Long.valueOf((Integer)claims.get("memberId"));
-		UserDetails userDetails = memberRepository.findMemberByMemberId(memberId);
+		String memberUuid = (String)claims.get("memberUuid");
+		UserDetails userDetails = memberRepository.findMemberByMemberUuid(memberUuid);
 		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 	}
 }
