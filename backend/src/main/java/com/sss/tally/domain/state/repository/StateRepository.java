169235e4ef11da97099d0670f63c3e8397e65b06
jungeunevepark <evecomcom98@naver.com>
2023-09-19 11:12:09 +0900

    :zap: feat: StateRepository 생성 및 state name으로 찾아오는 함수 선언
    Former-commit-id: beec3603c6a1b9a75078330df71528f489870833

---
+++ backend/src/main/java/com/sss/tally/domain/state/repository/StateRepository.java
@@ -0,0 +1,13 @@
+package com.sss.tally.domain.state.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sss.tally.domain.state.entity.State;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+	Optional<State> findStateByStateName(String stateName);
+	List<State> findAll();
+}
