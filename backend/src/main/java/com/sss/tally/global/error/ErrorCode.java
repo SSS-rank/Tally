d64b6615a93f551a723dff86e9494a001e942046
박현철 <wns882@naver.com>
2023-09-19 16:31:47 +0900

    :zap: feat: 정산 관련 error 코드 추가
    Former-commit-id: d318a05676bb8a4c71c196120d1f193c840a7e6c

--- backend/src/main/java/com/sss/tally/global/error/ErrorCode.java
+++ backend/src/main/java/com/sss/tally/global/error/ErrorCode.java
@@ -26,6 +26,8 @@ public enum ErrorCode {
 	NOT_BEFORE_STATUS(HttpStatus.BAD_REQUEST, "004", "정산 전 상태가 아닙니다."),
 	NOT_EQUAL_LOGIN_MEMBER_PAYER(HttpStatus.BAD_REQUEST, "005", "로그인 사용자와 결제자가 일치하지 않습니다."),
 	NOT_EXIST_GROUP_PAYMENT(HttpStatus.BAD_REQUEST, "006", "정산에 포함된 결제 정보들이 없습니다"),
+	NOT_VALID_CALCULATE_UUID(HttpStatus.BAD_REQUEST, "007", "유효하지 않은 정산 uuid 입니다."),
+	CANT_EQUAL_PAYER_REJECTOR(HttpStatus.BAD_REQUEST, "009", "결제자와 반려자가 같을 수 없습니다."),
 	//결제
 	NOT_EXIST_PAYMENT(HttpStatus.BAD_REQUEST, "001", "결제정보가 존재하지 않습니다.");
 
