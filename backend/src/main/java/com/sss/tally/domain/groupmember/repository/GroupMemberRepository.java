629d1c4b800ec716fd1ccc93f670864f7e90c1b7
박현철 <wns882@naver.com>
2023-09-19 14:52:16 +0900

    :zap: feat: GroupMember 리스트 찾는 기능 추가
    Former-commit-id: 377700c6fe4137621730664809e5cc4e3e684100

--- backend/src/main/java/com/sss/tally/domain/groupmember/repository/GroupMemberRepository.java
+++ backend/src/main/java/com/sss/tally/domain/groupmember/repository/GroupMemberRepository.java
@@ -1,9 +1,12 @@
 package com.sss.tally.domain.groupmember.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sss.tally.domain.groupmember.entity.GroupMember;
+import com.sss.tally.domain.member.entity.Member;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Integer> {
-
+	List<GroupMember> findGroupMembersByMemberId(Member member);
 }
