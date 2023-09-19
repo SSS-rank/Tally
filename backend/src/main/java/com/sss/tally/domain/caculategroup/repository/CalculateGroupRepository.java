742dbdc0b904a91c4182f23d61faf8b2b683ec24
박현철 <wns882@naver.com>
2023-09-19 16:31:31 +0900

    :zap: feat: CalculateGroup을 uuid로 찾기
    Former-commit-id: 46eb4ccc8ac1273aabd8dafdaccb53d2ea7a546c

--- backend/src/main/java/com/sss/tally/domain/caculategroup/repository/CalculateGroupRepository.java
+++ backend/src/main/java/com/sss/tally/domain/caculategroup/repository/CalculateGroupRepository.java
@@ -9,4 +9,6 @@ import com.sss.tally.domain.member.entity.Member;
 
 public interface CalculateGroupRepository extends JpaRepository<CalculateGroup, Integer> {
 	List<CalculateGroup> findCalculateGroupsByMemberId(Member member);
+
+	CalculateGroup findCalculateGroupByCalculateGroupUuid(String uuid);
 }
