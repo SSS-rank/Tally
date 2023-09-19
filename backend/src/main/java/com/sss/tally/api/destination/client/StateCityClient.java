0948cae9f2511bd013824638ccebe365d268e2dd
jungeunevepark <evecomcom98@naver.com>
2023-09-19 11:12:27 +0900

    :zap: feat: 시도 정보를 받아오는 Feign client 구현
    Former-commit-id: 0dd0071796e9411e79c2ec6c20604c037f212e4b

--- backend/src/main/java/com/sss/tally/api/destination/client/StateCityClient.java
+++ backend/src/main/java/com/sss/tally/api/destination/client/StateCityClient.java
@@ -1,17 +1,17 @@
 package com.sss.tally.api.destination.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sss.tally.api.destination.dto.DestinationDto;
 
 @FeignClient(url = "https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app", name = "statusClient")
-public interface StatusClient {
+public interface StateCityClient {
 	@GetMapping(value="/v1/regcodes")
-	DestinationDto.StatusRespListDto requestStatusInfo(@RequestHeader("Content-type") String contentType,
-		@SpringQueryMap DestinationDto.StatusCityReqDto request);
+	DestinationDto.StateCityRespListDto requestStatusInfo(@RequestHeader("Content-type") String contentType,
+		@RequestParam(value = "is_ignore_zero") boolean isIgnoreZero, @RequestParam("regcode_pattern") String regcodePattern);
 
 
 }
