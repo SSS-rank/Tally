f6ab22340b5536f1b3b4223673a8fb60aebca40d
박현철 <wns882@naver.com>
2023-09-19 13:34:01 +0900

    :zap: feat: 그룹-결제 리스트 가져오는 기능 추가
    Former-commit-id: a9a425db755842659d7b896e085464962d031d5c

--- backend/src/main/java/com/sss/tally/domain/grouppayment/repository/GroupPaymentRepository.java
+++ backend/src/main/java/com/sss/tally/domain/grouppayment/repository/GroupPaymentRepository.java
@@ -1,10 +1,12 @@
 package com.sss.tally.domain.grouppayment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sss.tally.domain.caculategroup.entity.CalculateGroup;
 import com.sss.tally.domain.grouppayment.entity.GroupPayment;
 
 public interface GroupPaymentRepository extends JpaRepository<GroupPayment, Integer> {
-
+	List<GroupPayment> findGroupPaymentsByCalculateGroupId(CalculateGroup calculateGroup);
 }
