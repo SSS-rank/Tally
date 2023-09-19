9e95371fdc5e678b3de1253bdc05e712583a50df
박현철 <wns882@naver.com>
2023-09-19 13:32:34 +0900

    :zap: feat: CalculateGroup 리스트 찾는 기능 추가
    Former-commit-id: 3601f3cfd73e92b62c0495b0d114bdca87d9f0aa

--- backend/src/main/java/com/sss/tally/domain/caculategroup/repository/CalculateGroupRepository.java
+++ backend/src/main/java/com/sss/tally/domain/caculategroup/repository/CalculateGroupRepository.java
@@ -1,9 +1,12 @@
 package com.sss.tally.domain.caculategroup.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sss.tally.domain.caculategroup.entity.CalculateGroup;
+import com.sss.tally.domain.member.entity.Member;
 
 public interface CalculateGroupRepository extends JpaRepository<CalculateGroup, Integer> {
-
+	List<CalculateGroup> findCalculateGroupsByMemberId(Member member);
 }
