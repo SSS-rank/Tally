e908c5e838dfc1381fc6a2c0fde9c411a9983bd5
kosigi19 <kosigi19@naver.com>
2023-09-20 15:57:30 +0900

    ✨ feat: 계좌 관리 페이지 레이아웃 및 스타일 추가
    Former-commit-id: eb27ca793a522e021ca6e9d8d782237827591378

---
+++ frontend/app/screens/SettingScreen/ManageAccountScreen.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Button, Avatar } from 'react-native-paper';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { TextStyles } from '../../styles/CommonStyles';
+const ManageAccountScreen = ({ navigation }: any) => {
+	const [isCheck, setIsCheck] = useState(false);
+	return (
+		<View style={styles.viewContainer}>
+			<View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+				<Text style={{ ...TextStyles({ align: 'left', weight: 'bold' }).title }}>연결된 계좌</Text>
+				<Button
+					mode="text"
+					style={{ flex: 1, alignItems: 'flex-end' }}
+					onPress={() => navigation.navigate('AddAccount')}
+				>
+					계좌 추가
+				</Button>
+			</View>
+			<View
+				style={{
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginVertical: 15,
+					marginHorizontal: 5,
+				}}
+			>
+				<Avatar.Image
+					style={{ backgroundColor: 'transparent', marginHorizontal: 5 }}
+					size={48}
+					source={require('../../assets/images/kakao.png')}
+				/>
+				<View style={{ paddingHorizontal: 10, flex: 1 }}>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).medium,
+							lineHeight: 20,
+						}}
+					>
+						SSS뱅크 1002-111-222333
+					</Text>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).regular,
+							lineHeight: 20,
+						}}
+					>
+						104,082원
+					</Text>
+				</View>
+			</View>
+			<View
+				style={{
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginVertical: 15,
+					marginHorizontal: 5,
+				}}
+			>
+				<Avatar.Image
+					style={{ backgroundColor: 'transparent', marginHorizontal: 5 }}
+					size={48}
+					source={require('../../assets/images/kakao.png')}
+				/>
+				<View style={{ paddingHorizontal: 10, flex: 1 }}>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).medium,
+							lineHeight: 20,
+						}}
+					>
+						SSS뱅크 1002-111-222333
+					</Text>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).regular,
+							lineHeight: 20,
+						}}
+					>
+						104,082원
+					</Text>
+				</View>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	viewContainer: {
+		flex: 1,
+		backgroundColor: 'white',
+		paddingHorizontal: 15,
+	},
+});
+export default ManageAccountScreen;
