3ff7bf1653d25d8f1bf20c8f2d4b80a206a236d7
rladbwls30 <rladbwls30@naver.com>
2023-09-20 16:51:53 +0900

    :zap: feat: 계좌 잔액 불러오기, 삭제 로직 생성
    Former-commit-id: 91e0245531f7a88690e68f7072ebb3d7416806e7

--- backend/src/main/java/com/sss/tally/domain/account/service/AccountServiceImpl.java
+++ backend/src/main/java/com/sss/tally/domain/account/service/AccountServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sss.tally.api.account.dto.AccountDto;
+import com.sss.tally.domain.account.client.AccountInfoClient;
 import com.sss.tally.domain.account.entity.Account;
 import com.sss.tally.domain.account.repository.AccountRepository;
 import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.member.repository.MemberRepository;
 import com.sss.tally.global.error.ErrorCode;
+import com.sss.tally.global.error.exception.AccountException;
 import com.sss.tally.global.error.exception.MemberException;
 import com.sss.tally.global.util.SHA256Util;
 
@@ -23,12 +25,14 @@ import lombok.RequiredArgsConstructor;
 public class AccountServiceImpl implements AccountService{
 	private final AccountRepository accountRepository;
 	private final MemberRepository memberRepository;
+	private final AccountInfoClient accountInfoClient;
 	@Override
 	public void createAccount(Authentication authentication, AccountDto.AccountCreateReqDto accountCreateReqDto) throws
 		NoSuchAlgorithmException {
 		Member auth = (Member)authentication.getPrincipal();
 		Member member = memberRepository.findByMemberUuid(auth.getMemberUuid())
 			.orElseThrow(()->new MemberException(ErrorCode.NOT_EXIST_MEMBER));
+
 		Account account;
 		if(accountCreateReqDto.getOrderNumber()==1){ // 첫번째 계좌 등록
 			account = Account.of(member, accountCreateReqDto, true);
@@ -38,5 +42,23 @@ public class AccountServiceImpl implements AccountService{
 		} else account = Account.of(member, accountCreateReqDto, false);
 
 		accountRepository.save(account);
+	}
+
+	@Override
+	public void deleteAccount(Long accountId) {
+		Account account = accountRepository.findAccountByAccountIdAndStatusIsFalse(accountId)
+			.orElseThrow(()->new AccountException(ErrorCode.NOT_EXIST_ACCOUNT));
+		account.updateStatus(false);
+	}
+
+	@Override
+	public Long getBalance(Long accountId) {
+		Account account = accountRepository.findAccountByAccountIdAndStatusIsFalse(accountId)
+			.orElseThrow(()->new AccountException(ErrorCode.NOT_EXIST_ACCOUNT));
+		String CONTENT_TYPE = "application/x-www-form-urlencoded;charset=utf-8";
+		AccountDto.AccountInfoReqDto accountInfoReqDto = AccountDto.AccountInfoReqDto.of(account);
+		AccountDto.AccountInfoRespDto accountInfoRespDto = accountInfoClient.getAccountBalance(
+			CONTENT_TYPE, accountInfoReqDto);
+		return accountInfoRespDto.getBalance();
 	}
 }
