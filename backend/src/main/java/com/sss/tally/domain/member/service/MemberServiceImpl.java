22364c0ef72a5f7fad4dfed80b2b6192a5d8105f
rladbwls30 <rladbwls30@naver.com>
2023-09-20 12:32:45 +0900

    :zap: feat: 회원 정보 변환을 위해 entity에서 멤버 받아오기
    Former-commit-id: 0191de613843894446710ae00875e552d1ba1c1f

--- backend/src/main/java/com/sss/tally/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/sss/tally/domain/member/service/MemberServiceImpl.java
@@ -12,6 +12,7 @@ import com.sss.tally.api.member.dto.MemberDto;
 import com.sss.tally.domain.device.entity.Device;
 import com.sss.tally.domain.device.repository.DeviceRepository;
 import com.sss.tally.domain.member.entity.Member;
+import com.sss.tally.domain.member.repository.MemberRepository;
 import com.sss.tally.global.error.ErrorCode;
 import com.sss.tally.global.error.exception.MemberException;
 import com.sss.tally.global.redis.service.RedisService;
@@ -24,10 +25,13 @@ import lombok.RequiredArgsConstructor;
 public class MemberServiceImpl implements MemberService{
 	private final RedisService redisService;
 	private final DeviceRepository deviceRepository;
+	private final MemberRepository memberRepository;
 
 	@Override
 	public void withdrawal(Authentication authentication) {
-		Member member = (Member)authentication.getPrincipal();
+		Member auth = (Member)authentication.getPrincipal();
+		Member member = memberRepository.findByMemberUuid(auth.getMemberUuid())
+			.orElseThrow(()->new MemberException(ErrorCode.NOT_EXIST_MEMBER));
 		// redis에 저장된 refreshtoken 만료 처리
 		String refreshToken = redisService.getValues(member.getMemberUuid());
 		if(StringUtils.hasText(refreshToken)){
@@ -50,7 +54,9 @@ public class MemberServiceImpl implements MemberService{
 
 	@Override
 	public MemberDto.MemberRespDto patchMemberInfo(Authentication authentication, MemberDto.MemberReqDto memberReqDto) {
-		Member member = (Member)authentication.getPrincipal();
+		Member auth = (Member)authentication.getPrincipal();
+		Member member = memberRepository.findByMemberUuid(auth.getMemberUuid())
+				.orElseThrow(()->new MemberException(ErrorCode.NOT_EXIST_MEMBER));
 		member.patchMemberInfo(memberReqDto.getNickname(), memberReqDto.getProfileImage());
 		return MemberDto.MemberRespDto.of(member.getNickname(), member.getProfileImage());
 	}
