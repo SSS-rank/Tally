4374177f9afaadb223d9f08104ad9cb56da9be50
박현철 <wns882@naver.com>
2023-09-20 12:38:05 +0900

    :zap: feat: 요청받은 정산 상세내용 보기 JPQL작성
    Former-commit-id: 7c26aaf424750cac665a96317f2407a5ff3e6a29

--- backend/src/main/java/com/sss/tally/domain/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/sss/tally/domain/payment/repository/PaymentRepository.java
@@ -3,9 +3,27 @@ package com.sss.tally.domain.payment.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.sss.tally.api.calculate.dto.CalculateDto;
+import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.payment.entity.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 	Optional<Payment> findPaymentByPaymentUuid(String uuid);
+
+	@Query("SELECT NEW com.sss.tally.api.calculate.dto.CalculateDto$Detail(" +
+		"P.paymentName, " +
+		"MP.amount, " +
+		"P.amount, " +
+		"P.paymentLocalDate) " +
+		"FROM Payment P " +
+		"INNER JOIN MemberPayment MP ON MP.paymentId = P " +
+		"WHERE P = :payment " +
+		"AND MP.memberId = :memberId " +
+		"AND P.status = false " +
+		"AND MP.status = false ")
+	CalculateDto.Detail findDetail(@Param("payment") Payment payment, @Param("memberId") Member memberId);
+
 }
