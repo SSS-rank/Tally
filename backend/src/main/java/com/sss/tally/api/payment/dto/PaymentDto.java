71a4616abf53eaf2c6cc4066fbc521a97fa1d4a6
jungeunevepark <evecomcom98@naver.com>
2023-09-20 15:27:38 +0900

    :zap: feat: 결제 수동 생성 시, 사용되는 Dto 구현
    Former-commit-id: 0fe778e3ba6c37b70908ca723f3d5c338d5cfcf6

---
+++ backend/src/main/java/com/sss/tally/api/payment/dto/PaymentDto.java
@@ -0,0 +1,50 @@
+package com.sss.tally.api.payment.dto;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sss.tally.api.memberpayment.dto.MemberPaymentDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class PaymentDto {
+
+	@Getter
+	@Builder
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public static class PaymentManualDto{
+
+		@NotNull
+		private Long travelId;
+
+		@NotNull
+		private Double amount;
+		@NotNull
+		private Double ratio;
+		@NotNull
+		private Long paymentUnitId;
+
+		private Long category;
+		@NotNull
+		private String paymentDateTime;
+
+		private String memo;
+		@NotNull
+		private boolean visible;
+
+		private String paymentType;
+
+		private String title;
+
+		private List<MemberPaymentDto.MemberPaymentCreateDto> paymentParticipants;
+	}
+
+}
