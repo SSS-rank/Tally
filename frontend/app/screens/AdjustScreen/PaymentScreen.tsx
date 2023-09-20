93b0868c2bcfbfbda4530c2d066f64fb99340653
kosigi19 <kosigi19@naver.com>
2023-09-20 14:26:06 +0900

    💄 style: 계좌 체크 박스 추가
    Former-commit-id: 053c773fd0c966d41c4850860d67eeb0238f39bf

--- frontend/app/screens/AdjustScreen/PaymentScreen.tsx
+++ frontend/app/screens/AdjustScreen/PaymentScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Button, Avatar } from 'react-native-paper';
 
@@ -6,20 +6,23 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 import { TextStyles } from '../../styles/CommonStyles';
 const PaymentScreen = () => {
+	const [isCheck, setIsCheck] = useState(false);
 	return (
 		<View style={styles.viewContainer}>
-			<Text style={TextStyles({ align: 'left' }).title}>결제할 계좌 선택</Text>
+			<Text style={{ ...TextStyles({ align: 'left', weight: 'bold' }).title, marginVertical: 10 }}>
+				결제할 계좌 선택
+			</Text>
 			<View
 				style={{
 					flexDirection: 'row',
 					alignItems: 'center',
-					marginVertical: 20,
+					marginVertical: 15,
 					marginHorizontal: 5,
 				}}
 			>
 				<Avatar.Image
-					style={{ backgroundColor: 'transparent' }}
-					size={54}
+					style={{ backgroundColor: 'transparent', marginHorizontal: 5 }}
+					size={48}
 					source={require('../../assets/images/kakao.png')}
 				/>
 				<View style={{ paddingHorizontal: 10, flex: 1 }}>
@@ -40,21 +43,103 @@ const PaymentScreen = () => {
 						104,082원
 					</Text>
 				</View>
-				{/* <Text
-					style={{
-						...TextStyles({ align: 'left' }).regular,
-						lineHeight: 20,
+				<Icon
+					name={isCheck ? 'checkmark-circle' : 'checkmark-circle-outline'}
+					size={32}
+					color={isCheck ? '#91C0EB' : '#D0D0D0'}
+					style={{ marginLeft: 5 }}
+					onPress={() => {
+						setIsCheck(!isCheck);
 					}}
-				>
-					check
-				</Text> */}
-				<Icon name="checkmark-circle" size={32} color="#91C0EB" style={{ marginLeft: 5 }} />
-				{/* <Icon name="checkmark-circle" size={32} color="#D0D0D0" style={{ marginLeft: 5 }} /> */}
-				<Icon name="checkmark-circle-outline" size={32} color="#D0D0D0" style={{ marginLeft: 5 }} />
+				/>
 			</View>
-			<Button mode="elevated" buttonColor="#91C0EB" textColor="white">
-				완료
-			</Button>
+			<View
+				style={{
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginVertical: 15,
+					marginHorizontal: 5,
+				}}
+			>
+				<Avatar.Image
+					style={{ backgroundColor: 'transparent', marginHorizontal: 5 }}
+					size={48}
+					source={require('../../assets/images/kakao.png')}
+				/>
+				<View style={{ paddingHorizontal: 10, flex: 1 }}>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).regular,
+							lineHeight: 20,
+						}}
+					>
+						SSS뱅크 1002-111-222333
+					</Text>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).medium,
+							lineHeight: 20,
+						}}
+					>
+						104,082원
+					</Text>
+				</View>
+				<Icon
+					name={isCheck ? 'checkmark-circle' : 'checkmark-circle-outline'}
+					size={32}
+					color={isCheck ? '#91C0EB' : '#D0D0D0'}
+					style={{ marginLeft: 5 }}
+					onPress={() => {
+						setIsCheck(!isCheck);
+					}}
+				/>
+			</View>
+			<View
+				style={{
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginVertical: 20,
+					marginHorizontal: 5,
+				}}
+			>
+				<Avatar.Image
+					style={{ backgroundColor: 'transparent', marginHorizontal: 5 }}
+					size={48}
+					source={require('../../assets/images/kakao.png')}
+				/>
+				<View style={{ paddingHorizontal: 10, flex: 1 }}>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).regular,
+							lineHeight: 20,
+						}}
+					>
+						SSS뱅크 1002-111-222333
+					</Text>
+					<Text
+						style={{
+							...TextStyles({ align: 'left' }).medium,
+							lineHeight: 20,
+						}}
+					>
+						104,082원
+					</Text>
+				</View>
+				<Icon
+					name={isCheck ? 'checkmark-circle' : 'checkmark-circle-outline'}
+					size={32}
+					color={isCheck ? '#91C0EB' : '#D0D0D0'}
+					style={{ marginLeft: 5 }}
+					onPress={() => {
+						setIsCheck(!isCheck);
+					}}
+				/>
+			</View>
+			<View style={{ justifyContent: 'flex-end', flex: 1, marginVertical: 30 }}>
+				<Button mode="elevated" buttonColor="#91C0EB" textColor="white">
+					완료
+				</Button>
+			</View>
 		</View>
 	);
 };
