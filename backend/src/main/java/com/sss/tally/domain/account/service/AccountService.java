539133174b9c305b0235464cd9535ec77863e560
rladbwls30 <rladbwls30@naver.com>
2023-09-20 12:29:47 +0900

    :zap: feat: 계좌 등록하는 로직 추가
    Former-commit-id: 1ddb93051ea03133ef0a99b865aa5d7dc9c0b2a3

--- backend/src/main/java/com/sss/tally/domain/account/service/AccountService.java
+++ backend/src/main/java/com/sss/tally/domain/account/service/AccountService.java
@@ -1,7 +1,12 @@
 package com.sss.tally.domain.account.service;
 
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.security.core.Authentication;
+
 import com.sss.tally.api.account.dto.AccountDto;
 
 public interface AccountService {
-	void createAccount(AccountDto.AccountCreateReqDto accountCreateReqDto);
+	void createAccount(Authentication authentication, AccountDto.AccountCreateReqDto accountCreateReqDto) throws
+		NoSuchAlgorithmException;
 }
