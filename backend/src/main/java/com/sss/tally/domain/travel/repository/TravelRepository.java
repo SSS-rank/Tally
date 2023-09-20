10772b7926fa9070add0a0b93b437a9b235c0f05
jungeunevepark <evecomcom98@naver.com>
2023-09-20 10:55:35 +0900

    :zap: feat: 다가오는 여행, 마친 여행, 진행중인 여행 리스트를 받는 함수 선언 및 구현
    Former-commit-id: 4f6167353de8e149f9d7f0a6a43d03b27af989ba

--- backend/src/main/java/com/sss/tally/domain/travel/repository/TravelRepository.java
+++ backend/src/main/java/com/sss/tally/domain/travel/repository/TravelRepository.java
@@ -1,8 +1,30 @@
 package com.sss.tally.domain.travel.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.travel.entity.Travel;
 
 public interface TravelRepository extends JpaRepository<Travel, Long> {
+
+	@Query("SELECT t FROM Travel t WHERE t.travelId IN " +
+		"(SELECT tg.travelId FROM TravelGroup tg WHERE tg.memberId = :memberId) " +
+		"AND t.endDate < :now")
+	List<Travel> findUpcomingTravelForMember(@Param("memberId") Member memberId, @Param("now") LocalDate now);
+
+
+	@Query("SELECT t FROM Travel t WHERE t.travelId IN " +
+		"(SELECT tg.travelId FROM TravelGroup tg WHERE tg.memberId = :memberId) " +
+		"AND t.startDate <= :now AND t.endDate >= :now")
+	List<Travel> findOngoingTravelForMember(@Param("memberId") Member memberId, @Param("now") LocalDate now);
+
+	@Query("SELECT t FROM Travel t WHERE t.travelId IN " +
+		"(SELECT tg.travelId FROM TravelGroup tg WHERE tg.memberId = :memberId) " +
+		"AND t.startDate > :now")
+	List<Travel> findPastTravelForMember(@Param("memberId") Member memberId, @Param("now") LocalDate now);
 }
