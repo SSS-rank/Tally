f67eefc9bd2c808dcac14c1e48b3f989120de883
rladbwls30 <rladbwls30@naver.com>
2023-09-20 12:29:33 +0900

    :zap: feat: 계좌 등록시 받아오는 DTO 생성, 비밀번호는 첫 등록시에만 받아오기
    Former-commit-id: 7bc705a47443de6180c7af8b0b42e50855f939d2

--- backend/src/main/java/com/sss/tally/api/account/dto/AccountDto.java
+++ backend/src/main/java/com/sss/tally/api/account/dto/AccountDto.java
@@ -1,5 +1,11 @@
 package com.sss.tally.api.account.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,8 +14,15 @@ import lombok.Getter;
 public class AccountDto {
 	@Getter
 	@Builder
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static class AccountCreateReqDto{
+		@NotBlank
 		private String accountNumber;
-	}
+		@NotNull
+		private String bankName;
+		@NotNull
+		private int orderNumber;
 
+		private String transferPassword;
+	}
 }
