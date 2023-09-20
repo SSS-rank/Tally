8ea6bfc4a1bff30781b3922ec5f4bb918b31855d
rladbwls30 <rladbwls30@naver.com>
2023-09-20 12:53:50 +0900

    :zap: feat: 저장된 salt 값으로 비밀번호 암호화해서 교체
    Former-commit-id: 28cc0bad084e723e57ffab4b68a8d0e4dbb20470

--- backend/src/main/java/com/sss/tally/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/sss/tally/domain/member/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sss.tally.domain.member.service;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -16,6 +17,7 @@ import com.sss.tally.domain.member.repository.MemberRepository;
 import com.sss.tally.global.error.ErrorCode;
 import com.sss.tally.global.error.exception.MemberException;
 import com.sss.tally.global.redis.service.RedisService;
+import com.sss.tally.global.util.SHA256Util;
 
 import lombok.RequiredArgsConstructor;
 
@@ -59,5 +61,15 @@ public class MemberServiceImpl implements MemberService{
 				.orElseThrow(()->new MemberException(ErrorCode.NOT_EXIST_MEMBER));
 		member.patchMemberInfo(memberReqDto.getNickname(), memberReqDto.getProfileImage());
 		return MemberDto.MemberRespDto.of(member.getNickname(), member.getProfileImage());
+	}
+
+	@Override
+	public void patchPassword(Authentication authentication, MemberDto.MemberPasswordDto memberPasswordDto) throws
+		NoSuchAlgorithmException {
+		Member auth = (Member)authentication.getPrincipal();
+		Member member = memberRepository.findByMemberUuid(auth.getMemberUuid())
+			.orElseThrow(()->new MemberException(ErrorCode.NOT_EXIST_MEMBER));
+		String password = SHA256Util.getEncrypt(memberPasswordDto.getTransferPassword(), member.getTransferSalt());
+		member.patchPassword(password);
 	}
 }
