4294387648998bf447d520cdfc3f582f18fa3b12
이다곤 <leedagon624@gmail.com>
2023-09-22 01:36:37 +0900

    :zap: feat: 결제 내역 인터페이스 추가
    Former-commit-id: c7c2b2b8f61c5fafc9f17a91855a6374071299a8

--- frontend/app/model/payment.ts
+++ frontend/app/model/payment.ts
@@ -11,6 +11,18 @@ export interface DirectPayReq {
 	travel_id: number; // 여행 id (Long)
 	visible: boolean;
 }
+export interface Payment {
+	payment_uuid: string; // 결제 uuid (string)
+	category_id: number; // 카테고리 id(Long)
+	amount: number; // 결제 총 금액(int)
+	payment_date: string; // 결제 시간(String)
+	payment_memo: string; // 결제 상세 메모(String)
+	payment_method: string; // 결제 방식(String)
+	payment_unit: string; // 결제 단위(String)
+	visible: boolean; // 공개여부(boolean)
+	payment_name: string; // 예금주(String)
+	calculate_status: string;
+}
 
 export interface DirectPayMember {
 	amount: number;
