99b463c309b264f65f752a783a9cace4c1160114
jungeunevepark <evecomcom98@naver.com>
2023-09-21 16:38:28 +0900

    :pencil2: fix: 결제 내역을 받아오는 feign client 반환 형식 수정
    Former-commit-id: b6befb9fa38bc93f1be97d9c89066c077e108e06

--- backend/src/main/java/com/sss/tally/domain/payment/client/PaymentClient.java
+++ backend/src/main/java/com/sss/tally/domain/payment/client/PaymentClient.java
@@ -1,18 +1,16 @@
 package com.sss.tally.domain.payment.client;
 
-import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.sss.tally.api.payment.dto.PaymentDto;
 
-@FeignClient(url = "http://j9a108.p.ssafy.io:8011/", name = "paymentClient")
+@FeignClient(url = "http://j9a108.p.ssafy.io:8011", name = "paymentClient")
 public interface PaymentClient {
-	@GetMapping(value="/transfer/history/tally", consumes = "application/json")
-	List<PaymentDto.PaymentListRespDto> requestTransferList(@RequestHeader("Content-type") String contentType,
-		@SpringQueryMap PaymentDto.PaymentListReqDto paymentListReqDto);
+	@PostMapping(value="/transfer/history/tally", consumes = "application/json")
+	PaymentDto.PaymentResDto requestTransferList(@RequestHeader("Content-type") String contentType,
+		@RequestBody PaymentDto.PaymentListReqDto paymentListReqDto);
 
 }
