118b16ac12dfc19b7c3c4feaf8ca61c5306fad8b
rladbwls30 <rladbwls30@naver.com>
2023-09-21 09:59:50 +0900

    :zap: feat: 정해진 순서대로 계좌 리스트 가져오기
    Former-commit-id: c1627a402a439b10b87afee2e37fa5517feae757

--- backend/src/main/java/com/sss/tally/domain/account/repository/AccountRepository.java
+++ backend/src/main/java/com/sss/tally/domain/account/repository/AccountRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sss.tally.domain.account.entity.Account;
+import com.sss.tally.domain.member.entity.Member;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	Optional<Account> findAccountByAccountNumberAndStatusIsFalse(String accountNumber);
@@ -13,4 +14,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 	Optional<Account> findAccountByAccountIdAndStatusIsFalse(Long accountId);
 
 	List<Account> findAllByMemberId_MemberUuid(String memberUuid);
+
+	List<Account> findAllByMemberIdAndStatusIsFalseOrderByOrderNumberAsc(Member member);
 }
