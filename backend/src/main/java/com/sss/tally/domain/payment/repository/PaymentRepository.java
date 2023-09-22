0e5c435c73c25ee58257724cc135c1284a56e6c7
rladbwls30 <rladbwls30@naver.com>
2023-09-22 09:55:17 +0900

    :zap: feat: 여행 별 결제 내역 가져오는 쿼리 문 작성
    Former-commit-id: 11567bfdaacd7666aeec0d02768ba784ce283a55

--- backend/src/main/java/com/sss/tally/domain/payment/repository/PaymentRepository.java
+++ backend/src/main/java/com/sss/tally/domain/payment/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.sss.tally.domain.payment.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import com.sss.tally.api.calculate.dto.CalculateDto;
 import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.payment.entity.Payment;
+import com.sss.tally.domain.travel.entity.Travel;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 	Optional<Payment> findPaymentByPaymentUuidAndStatusIsFalse(String uuid);
@@ -28,4 +30,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 		"AND MP.status = false ")
 	CalculateDto.Detail findDetail(@Param("payment") Payment payment, @Param("memberId") Member memberId);
 
+	List<Payment> findAllByTravelIdAndMemberIdAndStatusIsFalse(Travel travel, Member member);
+
+	List<Payment> findAllByTravelIdAndMemberIdAndStatusIsFalseAndVisibleIsTrue(Travel travel, Member member);
 }
