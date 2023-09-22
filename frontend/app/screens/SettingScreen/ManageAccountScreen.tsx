14dc7f24b43476fc1c6451cacb12734942fc3773
zero9657@naver.com <zero9657@naver.com>
2023-09-22 16:21:54 +0900

    ✏️ fix : 계좌 목록 조회 api 결과 recoil에 저장, 데이터 변경 시 리랜더링
    Former-commit-id: 3896b6da9e3ee001587b34f25f431ebfacd11faf

--- frontend/app/screens/SettingScreen/ManageAccountScreen.tsx
+++ frontend/app/screens/SettingScreen/ManageAccountScreen.tsx
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Text, View, StyleSheet, FlatList } from 'react-native';
 import { Button } from 'react-native-paper';
 
+import { useFocusEffect } from '@react-navigation/native';
+import { useRecoilState } from 'recoil';
+
 import AccountItem from '../../components/AccountItem/AccountItem';
 import useAxiosWithAuth from '../../hooks/useAxiosWithAuth';
-import { Account } from '../../model/account';
+import { tallyAccountListState } from '../../recoil/recoil';
 import { TextStyles } from '../../styles/CommonStyles';
 const ManageAccountScreen = ({ navigation }: any) => {
 	const api = useAxiosWithAuth();
-	const [accountList, setAccountList] = useState<Account[]>();
-	useEffect(() => {
-		getAccountList();
-	}, []);
+	const [accountListState, setAccountListState] = useRecoilState(tallyAccountListState);
+	useFocusEffect(
+		useCallback(() => {
+			getAccountList();
+		}, [accountListState]),
+	);
 
 	const getAccountList = async () => {
 		try {
 			const res = await api.get(`/account`);
 
 			if (res.status === 200) {
-				setAccountList(res.data);
+				setAccountListState(res.data);
 			}
 		} catch (err) {
 			console.error(err);
@@ -37,7 +42,7 @@ const ManageAccountScreen = ({ navigation }: any) => {
 				</Button>
 			</View>
 			<FlatList
-				data={accountList}
+				data={accountListState}
 				renderItem={({ item }) => (
 					<AccountItem
 						key={item.accountNumber}
