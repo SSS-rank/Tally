aa71b6774efd1f601ae97d233da1ef391e82617f
rladbwls30 <rladbwls30@naver.com>
2023-09-22 15:14:31 +0900

    :pencil2: fix: 파일이랑 request 보내기 위해 parameter 수정
    Former-commit-id: f9c77acb9fff6b1be624b2e56de27b5c30b497c1

--- backend/src/main/java/com/sss/tally/api/member/controller/MemberController.java
+++ backend/src/main/java/com/sss/tally/api/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.sss.tally.api.member.controller;
 
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sss.tally.api.member.dto.MemberDto;
 import com.sss.tally.domain.member.service.MemberService;
@@ -35,8 +38,10 @@ public class MemberController {
 	}
 
 	@PatchMapping("/profile")
-	public ResponseEntity<MemberDto.MemberRespDto> patchMemberInfo(Authentication authentication, MemberDto.MemberReqDto memberReqDto){
-		MemberDto.MemberRespDto memberRespDto = memberService.patchMemberInfo(authentication, memberReqDto);
+	public ResponseEntity<MemberDto.MemberRespDto> patchMemberInfo(Authentication authentication,
+		@RequestPart(value="key", required = true) MemberDto.MemberReqDto memberReqDto, @RequestPart(value="file", required = false)
+		MultipartFile file) throws IOException {
+		MemberDto.MemberRespDto memberRespDto = memberService.patchMemberInfo(authentication, memberReqDto.getNickname(), file);
 		return ResponseEntity.status(HttpStatus.OK).body(memberRespDto);
 	}
 
