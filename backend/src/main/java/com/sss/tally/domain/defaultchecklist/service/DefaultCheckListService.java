612f23cf2358e827d0b6668ff1deda0a8b4d355f
박현철 <wns882@naver.com>
2023-09-22 17:48:54 +0900

    :zap: feat: 기본 체크리스트 초기 생성 기능 추가
    Former-commit-id: 26086871534fec1433fb0a9616aabb58679adcf6

---
+++ backend/src/main/java/com/sss/tally/domain/defaultchecklist/service/DefaultCheckListService.java
@@ -0,0 +1,8 @@
+package com.sss.tally.domain.defaultchecklist.service;
+
+import com.sss.tally.domain.member.entity.Member;
+
+public interface DefaultCheckListService {
+
+	public void createInitCheckList(Member member);
+}
