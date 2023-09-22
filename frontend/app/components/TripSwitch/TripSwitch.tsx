1b2068cb86a41491bc332bc3858a5ee98120a796
zero9657@naver.com <zero9657@naver.com>
2023-09-22 09:29:04 +0900

    ✏️ fix : after, before 변수 서로 교체
    Former-commit-id: 85bf4baa84cc5ba09431274bc3cebf21ab5123a3

--- frontend/app/components/TripSwitch/TripSwitch.tsx
+++ frontend/app/components/TripSwitch/TripSwitch.tsx
@@ -12,15 +12,15 @@ function TripSwitch({ selectionMode, setSelectionMode }: TripSwitchProp) {
 			<View style={styles.switchContainer}>
 				<TouchableOpacity
 					activeOpacity={1}
-					onPress={() => setSelectionMode('before')}
+					onPress={() => setSelectionMode('after')}
 					style={{
 						...styles.switchInner,
-						backgroundColor: selectionMode == 'before' ? '#91C0EB' : 'white',
+						backgroundColor: selectionMode == 'after' ? '#91C0EB' : 'white',
 					}}
 				>
 					<Text
 						style={{
-							color: selectionMode == 'before' ? 'white' : '#91C0EB',
+							color: selectionMode == 'after' ? 'white' : '#91C0EB',
 						}}
 					>
 						여행 전
@@ -44,15 +44,15 @@ function TripSwitch({ selectionMode, setSelectionMode }: TripSwitchProp) {
 				</TouchableOpacity>
 				<TouchableOpacity
 					activeOpacity={1}
-					onPress={() => setSelectionMode('after')}
+					onPress={() => setSelectionMode('before')}
 					style={{
 						...styles.switchInner,
-						backgroundColor: selectionMode == 'after' ? '#91C0EB' : 'white',
+						backgroundColor: selectionMode == 'before' ? '#91C0EB' : 'white',
 					}}
 				>
 					<Text
 						style={{
-							color: selectionMode == 'after' ? 'white' : '#91C0EB',
+							color: selectionMode == 'before' ? 'white' : '#91C0EB',
 						}}
 					>
 						여행 후
