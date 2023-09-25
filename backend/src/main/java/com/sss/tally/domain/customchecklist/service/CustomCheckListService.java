1425d7eab43056972ef0e81d5a900070c0b6c760
박현철 <wns882@naver.com>
2023-09-25 10:59:11 +0900

    :zap: feat: 커스텀 체크리스트 저장 기능 service 추가
    Former-commit-id: 1e1b9c0f61c5c01495f4f7d538bed22ff0a43780

--- backend/src/main/java/com/sss/tally/domain/customchecklist/service/CustomCheckListService.java
+++ backend/src/main/java/com/sss/tally/domain/customchecklist/service/CustomCheckListService.java
@@ -1,9 +1,12 @@
 package com.sss.tally.domain.customchecklist.service;
 
+import com.sss.tally.api.customchecklist.dto.CustomCheckListDto;
 import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.travel.entity.Travel;
 
 public interface CustomCheckListService {
 
 	public void createInitCustomCheckList(Member member, Travel travel);
+
+	String addContent(String memberUuid, CustomCheckListDto.AddCustomCheckListReqDto addCustomCheckListReqDto);
 }
