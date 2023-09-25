ef100f39114d9f344acd0803a14b32fc101acc54
박현철 <wns882@naver.com>
2023-09-25 11:21:20 +0900

    :zap: feat: 커스텀 체크리스트ID로 체크리스트 가져오는 메서드 추가
    Former-commit-id: 9e7258fca7b32a26eb00bf5171281825f605e7b0

--- backend/src/main/java/com/sss/tally/domain/customchecklist/repository/CustomCheckListRepository.java
+++ backend/src/main/java/com/sss/tally/domain/customchecklist/repository/CustomCheckListRepository.java
@@ -1,9 +1,12 @@
 package com.sss.tally.domain.customchecklist.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sss.tally.domain.customchecklist.entity.CustomChecklist;
 
 public interface CustomCheckListRepository extends JpaRepository<CustomChecklist, Integer> {
 
+	Optional<CustomChecklist> findCustomChecklistByCustomChecklistId(Long customCheckListId);
 }
