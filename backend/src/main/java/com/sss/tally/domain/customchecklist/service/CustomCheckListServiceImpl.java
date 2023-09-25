cac64a1731213e31f9beb9fac25c86df2efcec64
박현철 <wns882@naver.com>
2023-09-25 10:59:17 +0900

    :zap: feat: 커스텀 체크리스트 저장 기능 추가
    Former-commit-id: d5bed04f8411c226a556709fc27b1d18b672eebc

--- backend/src/main/java/com/sss/tally/domain/customchecklist/service/CustomCheckListServiceImpl.java
+++ backend/src/main/java/com/sss/tally/domain/customchecklist/service/CustomCheckListServiceImpl.java
@@ -1,18 +1,25 @@
 package com.sss.tally.domain.customchecklist.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sss.tally.api.customchecklist.dto.CustomCheckListDto;
 import com.sss.tally.domain.customchecklist.entity.CustomChecklist;
 import com.sss.tally.domain.customchecklist.repository.CustomCheckListRepository;
 import com.sss.tally.domain.defaultchecklist.entity.DefaultCheckList;
 import com.sss.tally.domain.defaultchecklist.repository.DefaultCheckListRepository;
 import com.sss.tally.domain.member.entity.Member;
+import com.sss.tally.domain.member.repository.MemberRepository;
 import com.sss.tally.domain.travel.entity.Travel;
+import com.sss.tally.domain.travel.repository.TravelRepository;
+import com.sss.tally.domain.travelgroup.repository.TravelGroupRepository;
 import com.sss.tally.global.error.ErrorCode;
 import com.sss.tally.global.error.exception.CalculateException;
+import com.sss.tally.global.error.exception.MemberException;
+import com.sss.tally.global.error.exception.TravelException;
 
 import lombok.AllArgsConstructor;
 
@@ -25,6 +32,12 @@ public class CustomCheckListServiceImpl implements CustomCheckListService {
 
 	private final CustomCheckListRepository customCheckListRepository;
 
+	private final MemberRepository memberRepository;
+
+	private final TravelRepository travelRepository;
+
+	private final TravelGroupRepository travelGroupRepository;
+
 	@Override
 	public void createInitCustomCheckList(Member member, Travel travel) {
 		List<DefaultCheckList> defaultCheckListList = defaultCheckListRepository.findDefaultCheckListByMemberId(
@@ -36,5 +49,27 @@ public class CustomCheckListServiceImpl implements CustomCheckListService {
 			customCheckListRepository.save(CustomChecklist.of(defaultCheckList, travel));
 		}
 
+	}
+
+	@Override
+	public String addContent(String memberUuid, CustomCheckListDto.AddCustomCheckListReqDto addCustomCheckListReqDto) {
+		Optional<Member> memberOptional = memberRepository.findMemberByMemberUuidAndWithdrawalDateIsNull(memberUuid);
+		if (memberOptional.isEmpty()) {
+			throw new MemberException(ErrorCode.ALREADY_WITHDRAWAL_MEMBER);
+		}
+		Member member = memberOptional.get();
+		Optional<Travel> travelOptional = travelRepository.findTravelByTravelId(addCustomCheckListReqDto.getTravelId());
+		if (travelOptional.isEmpty()) {
+			throw new TravelException(ErrorCode.NOT_EXIST_TRAVEL);
+		}
+		Travel travel = travelOptional.get();
+
+		boolean isMemberInTravel = travelGroupRepository.existsByTravelIdAndMemberId(travel, member);
+		if (!isMemberInTravel) {
+			throw new TravelException(ErrorCode.NOT_EXIST_MEMBER_TRAVEL);
+		}
+		customCheckListRepository.save(
+			CustomChecklist.forSaveOf(member, travel, addCustomCheckListReqDto.getContent()));
+		return "ok";
 	}
 }
