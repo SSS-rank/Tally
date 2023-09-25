2824eac536ebee5473d6e61709b9b7419fcc0c15
박현철 <wns882@naver.com>
2023-09-25 10:16:25 +0900

    :zap: feat: 체크리스트 삭제 기능 추가
    Former-commit-id: 1a2cef8ff5b6c9b05d2931d66dbe0b9a92be6a42

--- backend/src/main/java/com/sss/tally/api/defaultchecklist/controller/DefaultCheckListController.java
+++ backend/src/main/java/com/sss/tally/api/defaultchecklist/controller/DefaultCheckListController.java
@@ -3,7 +3,9 @@ package com.sss.tally.api.defaultchecklist.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +38,15 @@ public class DefaultCheckListController {
 		@AuthenticationPrincipal Member member) {
 		String memberUuid = member.getMemberUuid();
 		String result = defaultCheckListService.updateContent(memberUuid, updateDefaultCheckListReqDto);
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
+
+	@DeleteMapping("/{checkListId}")
+	public ResponseEntity<String> deleteContent(
+		@PathVariable Long checkListId,
+		@AuthenticationPrincipal Member member) {
+		String memberUuid = member.getMemberUuid();
+		String result = defaultCheckListService.deleteContent(memberUuid, checkListId);
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 }
