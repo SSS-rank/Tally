da6b3080db02af59ae31c7aadb0bd2de71729ccb
박현철 <wns882@naver.com>
2023-09-25 12:34:26 +0900

    :zap: feat: 커스텀 체크리스트 목록 가져오기 service 생성
    Former-commit-id: 1f20020bf768e75001f42920f5ab483188058b66

--- backend/src/main/java/com/sss/tally/domain/customchecklist/service/CustomCheckListService.java
+++ backend/src/main/java/com/sss/tally/domain/customchecklist/service/CustomCheckListService.java
@@ -1,5 +1,7 @@
 package com.sss.tally.domain.customchecklist.service;
 
+import java.util.List;
+
 import com.sss.tally.api.customchecklist.dto.CustomCheckListDto;
 import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.travel.entity.Travel;
@@ -13,4 +15,8 @@ public interface CustomCheckListService {
 	String updateContent(String memberUuid, CustomCheckListDto.UpdateCustomCheckListReqDto updateCustomCheckListReqDto);
 
 	String deleteContent(String memberUuid, Long checkListId);
+
+	List<CustomCheckListDto.GetCustomCheckListRespDto> getContent(String memberUuid, Long travelId);
+
+	String updateStatus(String memberUuid, Long checkListId);
 }
