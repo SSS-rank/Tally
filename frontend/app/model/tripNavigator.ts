7123099311495ad193f96a9ca2459cd5f6384f22
kosigi19 <kosigi19@naver.com>
2023-09-25 14:17:35 +0900

    ✨ feat: 정산 네비게이션 prop 모델 분리
    Former-commit-id: 323ee29cdc26550365cd2737b01763fd4de0de4c

---
+++ frontend/app/model/tripNavigator.ts
@@ -0,0 +1,7 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { TripStackProps } from '../navigation/TripStack';
+
+export type SendAdjustScreenProps = NativeStackScreenProps<TripStackProps, 'SendAdjust'>;
+export type GetAdjustScreenProps = NativeStackScreenProps<TripStackProps, 'GetAdjust'>;
+export type AdjustTripScreenProps = NativeStackScreenProps<TripStackProps, 'AdjustTrip'>;
