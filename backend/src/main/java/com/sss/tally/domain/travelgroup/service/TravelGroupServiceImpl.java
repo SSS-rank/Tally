b9396b331122d4de6b0994ba2bd16e4013dd06ae
jungeunevepark <evecomcom98@naver.com>
2023-09-25 13:03:12 +0900

    :zap: feat: TravelGroup의 공개 여부를 수정하는 함수 선언 및 구현
    Former-commit-id: 864782fa73fe5110e12b6b3dbf06a9b878fd1bf3

--- backend/src/main/java/com/sss/tally/domain/travelgroup/service/TravelGroupServiceImpl.java
+++ backend/src/main/java/com/sss/tally/domain/travelgroup/service/TravelGroupServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sss.tally.domain.travelgroup.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sss.tally.api.member.dto.MemberDto;
+import com.sss.tally.api.travel.dto.TravelDto;
 import com.sss.tally.domain.member.entity.Member;
 import com.sss.tally.domain.travel.entity.Travel;
 import com.sss.tally.domain.travel.repository.TravelRepository;
@@ -16,6 +18,7 @@ import com.sss.tally.domain.travelgroup.entity.TravelGroup;
 import com.sss.tally.domain.travelgroup.repository.TravelGroupRepository;
 import com.sss.tally.global.error.ErrorCode;
 import com.sss.tally.global.error.exception.TravelException;
+import com.sss.tally.global.error.exception.TravelGroupException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -49,5 +52,20 @@ public class TravelGroupServiceImpl implements TravelGroupService {
 		return memberIds.stream()
 			.map(MemberDto.MemberTravelDto::from)
 			.collect(Collectors.toList());
+	}
+
+	@Override
+	public void modifyTravelVisible(Authentication authentication, TravelDto.TravelVisibleReqDto travelVisibleReqDto) {
+		Member member = (Member) authentication.getPrincipal();
+
+		Optional<Travel> travelOptional = travelRepository.findTravelByTravelId(travelVisibleReqDto.getTravelId());
+		if(travelOptional.isEmpty()) throw new TravelException(ErrorCode.NOT_EXIST_TRAVEL);
+		if(travelOptional.get().getEndDate().isAfter(LocalDate.now()) || travelOptional.get().getStartDate().isAfter(LocalDate.now()))
+			throw new TravelException(ErrorCode.NOT_EDIT_VISIBLE);
+
+		Optional<TravelGroup> travelGroupOptional = travelGroupRepository.findTravelGroupByMemberIdAndTravelId(member, travelOptional.get());
+		if(travelGroupOptional.isEmpty()) throw new TravelGroupException(ErrorCode.NOT_EXIST_PARTICIPANT);
+
+		travelGroupOptional.get().updateVisible(travelVisibleReqDto.isVisible());
 	}
 }
