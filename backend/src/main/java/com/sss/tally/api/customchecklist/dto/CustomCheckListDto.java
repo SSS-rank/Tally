5ee1a5e59d3beb5547ca2956c49ee65a0686484a
박현철 <wns882@naver.com>
2023-09-25 10:59:02 +0900

    :zap: feat: 커스텀 체크리스트 저장 기능 dto 추가
    Former-commit-id: e51f39fc9111bb5bbdcae5e9e08e9ac5af3efa95

---
+++ backend/src/main/java/com/sss/tally/api/customchecklist/dto/CustomCheckListDto.java
@@ -0,0 +1,22 @@
+package com.sss.tally.api.customchecklist.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class CustomCheckListDto {
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	public static class AddCustomCheckListReqDto {
+		private String content;
+
+		public Long travelId;
+	}
+}
