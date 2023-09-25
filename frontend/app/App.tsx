8e9a746e86355aa6e7333ce00273ac77a7d8ea1d
zero9657@naver.com <zero9657@naver.com>
2023-09-25 23:40:24 +0900

    ✨ feat : 백그라운드, 포그라운드 시 알람 받기 로직 추가
    Former-commit-id: aad11c731b783a28c176e0d51992e118a731ba6e

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,32 +1,46 @@
-import React from 'react';
-import {MD3LightTheme, PaperProvider} from 'react-native-paper';
+import React, { useEffect } from 'react';
+import { MD3LightTheme, PaperProvider } from 'react-native-paper';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {RecoilRoot} from 'recoil';
+import messaging from '@react-native-firebase/messaging';
+import { NavigationContainer } from '@react-navigation/native';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 
 import RootStack from './navigation/RootStack';
 
+// 백그라운드일 때 알림 받기
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+	console.log('Message handled in the background!', remoteMessage);
+});
+
 const App = () => {
-  return (
-    <RecoilRoot>
-      <PaperProvider theme={theme}>
-        <NavigationContainer>
-          <RootStack />
-        </NavigationContainer>
-      </PaperProvider>
-    </RecoilRoot>
-  );
+	// 포그라운드일 때 알림 받기
+	useEffect(() => {
+		const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+			console.log('remoteMessage', JSON.stringify(remoteMessage));
+		});
+		return unsubscribe;
+	}, []);
+
+	return (
+		<RecoilRoot>
+			<PaperProvider theme={theme}>
+				<NavigationContainer>
+					<RootStack />
+				</NavigationContainer>
+			</PaperProvider>
+		</RecoilRoot>
+	);
 };
 
 const theme = {
-  ...MD3LightTheme, // or MD3DarkTheme
-  roundness: 2,
-  colors: {
-    ...MD3LightTheme.colors,
-    primary: '#3498db',
-    secondary: '#f1c40f',
-    tertiary: '#a1b2c3',
-  },
+	...MD3LightTheme, // or MD3DarkTheme
+	roundness: 2,
+	colors: {
+		...MD3LightTheme.colors,
+		primary: '#3498db',
+		secondary: '#f1c40f',
+		tertiary: '#a1b2c3',
+	},
 };
 
 export default App;
