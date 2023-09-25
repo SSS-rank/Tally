e1026449d6b27132e8cf5630c742a04e41b428ee
박현철 <wns882@naver.com>
2023-09-25 12:33:41 +0900

    :zap: feat: 커스텀 체크리스트 조회하기 컨트롤러 추가
    Former-commit-id: 4778cf4b8da4b223344a4d528f0ebb9942997406

--- backend/src/main/java/com/sss/tally/api/customchecklist/controller/CustomCheckListController.java
+++ backend/src/main/java/com/sss/tally/api/customchecklist/controller/CustomCheckListController.java
@@ -1,9 +1,12 @@
 package com.sss.tally.api.customchecklist.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +50,25 @@ public class CustomCheckListController {
 		@AuthenticationPrincipal Member member) {
 		String memberUuid = member.getMemberUuid();
 		String result = customCheckListService.deleteContent(memberUuid, checkListId);
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
+
+	@GetMapping("/{travelId}")
+	public ResponseEntity<List<CustomCheckListDto.GetCustomCheckListRespDto>> getContent(
+		@PathVariable Long travelId,
+		@AuthenticationPrincipal Member member) {
+		String memberUuid = member.getMemberUuid();
+		List<CustomCheckListDto.GetCustomCheckListRespDto> getCustomCheckListRespDtoList = customCheckListService.getContent(
+			memberUuid, travelId);
+		return ResponseEntity.status(HttpStatus.OK).body(getCustomCheckListRespDtoList);
+	}
+
+	@PatchMapping("/check/{checkListId}")
+	public ResponseEntity<String> updateStatus(
+		@PathVariable Long checkListId,
+		@AuthenticationPrincipal Member member) {
+		String memberUuid = member.getMemberUuid();
+		String result = customCheckListService.updateStatus(memberUuid, checkListId);
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 }
