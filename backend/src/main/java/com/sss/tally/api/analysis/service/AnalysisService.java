b3b54ba6cd9855001770281b112fa06e07bf9411
rladbwls30 <rladbwls30@naver.com>
2023-09-25 16:56:54 +0900

    :zap: feat: 여행에서 카테고리 별 리스트 보내주는 Service 생성
    Former-commit-id: cdf2e1aac0d72780d4622a80976f2ffc4305a336

--- backend/src/main/java/com/sss/tally/api/analysis/service/AnalysisService.java
+++ backend/src/main/java/com/sss/tally/api/analysis/service/AnalysisService.java
@@ -1,5 +1,7 @@
 package com.sss.tally.api.analysis.service;
 
+import java.util.List;
+
 import org.springframework.security.core.Authentication;
 
 import com.sss.tally.api.analysis.dto.AnalysisDto;
@@ -8,4 +10,6 @@ public interface AnalysisService {
 	AnalysisDto.GroupMemberRespDto getGroupAnalysis(Authentication authentication, Long travelId);
 
 	AnalysisDto.MemberRespDto getMemberAnalysis(Authentication authentication, Long travelId, String memberUuid);
+
+	List<AnalysisDto.CategoryRespDto> getCategoryDetail(Authentication authentication, Long travelId, String memberUuid, Long categoryId);
 }
