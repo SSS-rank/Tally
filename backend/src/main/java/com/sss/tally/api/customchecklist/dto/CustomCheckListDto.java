7dd89ff3145ce045398dec3f09617c412d76a8c2
박현철 <wns882@naver.com>
2023-09-25 12:33:47 +0900

    :zap: feat: 커스텀 체크리스트 조회하기 DTO 추가
    Former-commit-id: e65e8f6f8fa6cfa73bde7cf4716da2117a06c49f

--- backend/src/main/java/com/sss/tally/api/customchecklist/dto/CustomCheckListDto.java
+++ backend/src/main/java/com/sss/tally/api/customchecklist/dto/CustomCheckListDto.java
@@ -2,6 +2,7 @@ package com.sss.tally.api.customchecklist.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sss.tally.domain.customchecklist.entity.CustomChecklist;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,5 +31,24 @@ public class CustomCheckListDto {
 		private Long customCheckListId;
 
 		private String content;
+	}
+
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	public static class GetCustomCheckListRespDto {
+
+		private Long customCheckListId;
+
+		private String content;
+
+		public static CustomCheckListDto.GetCustomCheckListRespDto from(CustomChecklist customChecklist) {
+			return CustomCheckListDto.GetCustomCheckListRespDto.builder()
+				.customCheckListId(customChecklist.getCustomChecklistId())
+				.content(customChecklist.getContent())
+				.build();
+		}
 	}
 }
