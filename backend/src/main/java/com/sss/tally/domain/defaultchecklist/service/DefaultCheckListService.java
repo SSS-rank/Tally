b1eb567c1e3270e4b1a2d954c268de743791fa05
박현철 <wns882@naver.com>
2023-09-25 09:30:15 +0900

    :zap: feat: 기본 체크리스트 추가하기 Service 추가
    Former-commit-id: 51d104b375534f34dbe43e4061cdab7264aba4a9

--- backend/src/main/java/com/sss/tally/domain/defaultchecklist/service/DefaultCheckListService.java
+++ backend/src/main/java/com/sss/tally/domain/defaultchecklist/service/DefaultCheckListService.java
@@ -1,8 +1,11 @@
 package com.sss.tally.domain.defaultchecklist.service;
 
+import com.sss.tally.api.defaultchecklist.dto.DefaultCheckListDto;
 import com.sss.tally.domain.member.entity.Member;
 
 public interface DefaultCheckListService {
 
 	public void createInitCheckList(Member member);
+
+	String addContent(String memberUuid, DefaultCheckListDto.AddDefaultCheckListReqDto addDefaultCheckListReqDto);
 }
