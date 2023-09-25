4810a13e5ef257443dcfae7b03c3b35edc7599e4
박현철 <wns882@naver.com>
2023-09-25 10:03:08 +0900

    :zap: feat: 기본 체크리스트 업데이트 DTO 추가
    Former-commit-id: 7da8dfddd7398cb0d50f038a6ae22376f3329140

--- backend/src/main/java/com/sss/tally/api/defaultchecklist/dto/DefaultCheckListDto.java
+++ backend/src/main/java/com/sss/tally/api/defaultchecklist/dto/DefaultCheckListDto.java
@@ -1,16 +1,32 @@
 package com.sss.tally.api.defaultchecklist.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class DefaultCheckListDto {
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 	@NoArgsConstructor
 	@AllArgsConstructor
 	@Builder
 	@Getter
 	public static class AddDefaultCheckListReqDto {
+		private String content;
+	}
+
+	@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	public static class UpdateDefaultCheckListReqDto {
+
+		private Long defaultCheckListId;
+
 		private String content;
 	}
 }
