5e54866436053366b9df2c7fe277e82754e729b7
박현철 <wns882@naver.com>
2023-09-25 11:25:47 +0900

    :zap: feat: 커스텀 체크리스트 삭제하기 controller 추가
    Former-commit-id: 84e4b6b3199a857929cfcf22573e5f16e36efb6c

--- backend/src/main/java/com/sss/tally/api/customchecklist/controller/CustomCheckListController.java
+++ backend/src/main/java/com/sss/tally/api/customchecklist/controller/CustomCheckListController.java
@@ -3,7 +3,9 @@ package com.sss.tally.api.customchecklist.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,4 +41,12 @@ public class CustomCheckListController {
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
 
+	@DeleteMapping("/{checkListId}")
+	public ResponseEntity<String> deleteContent(
+		@PathVariable Long checkListId,
+		@AuthenticationPrincipal Member member) {
+		String memberUuid = member.getMemberUuid();
+		String result = customCheckListService.deleteContent(memberUuid, checkListId);
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
 }
