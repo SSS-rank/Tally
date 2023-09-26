f098e220a584b3fbd4c6dea552621cb927de687d
zero9657@naver.com <zero9657@naver.com>
2023-09-26 10:21:44 +0900

    ✨ feat : 현재 여행지 정보 출력
    Former-commit-id: 2e9d302e38b4b302a87ce3489b7afaf4bbca0bb3

--- frontend/app/screens/AnalysisScreen/AnalysisScreen.tsx
+++ frontend/app/screens/AnalysisScreen/AnalysisScreen.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+import { useRecoilValue } from 'recoil';
+
+import { CurTripInfoState } from '../../recoil/recoil';
+import { TextStyles } from '../../styles/CommonStyles';
 
 function AnalysisScreen() {
-  return (
-    <View style={styles.viewContainer}>
-      <Text>AnalysisScreen! 🎉</Text>
-    </View>
-  );
+	const curTripInfo = useRecoilValue(CurTripInfoState);
+	console.log(curTripInfo);
+	return (
+		<View style={styles.viewContainer}>
+			<View style={styles.topView}>
+				<View style={styles.top}>
+					<Text style={styles.title}>{curTripInfo.title}</Text>
+					<Text style={styles.info}>{curTripInfo.location}</Text>
+				</View>
+				<Text style={styles.info}>
+					{curTripInfo.startDay} ~ {curTripInfo.endDay}
+				</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  viewContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+	viewContainer: {
+		flex: 1,
+		padding: 15,
+		backgroundColor: '#ffffff',
+	},
+	topView: {},
+	top: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		marginBottom: 5,
+	},
+	title: {
+		...TextStyles({ align: 'left', weight: 'bold', mRight: 5 }).title,
+	},
+	info: {
+		...TextStyles({ align: 'left', color: '#666666' }).small,
+	},
 });
 
 export default AnalysisScreen;
