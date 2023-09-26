454bc9dd735c79cef92cf7a668a6369f7b64645f
zero9657@naver.com <zero9657@naver.com>
2023-09-26 14:14:37 +0900

    ✨ feat : 분석 그룹 api 연동해 차트 만들기
    Former-commit-id: cb91bb552616183317518f890089f53c564fc747

--- frontend/app/screens/AnalysisScreen/AnalysisScreen.tsx
+++ frontend/app/screens/AnalysisScreen/AnalysisScreen.tsx
@@ -1,13 +1,67 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { PieChart } from 'react-native-chart-kit';
 
+import { useFocusEffect } from '@react-navigation/native';
 import { useRecoilValue } from 'recoil';
 
+import useAxiosWithAuth from '../../hooks/useAxiosWithAuth';
 import { CurTripInfoState } from '../../recoil/recoil';
 import { TextStyles } from '../../styles/CommonStyles';
 
+interface charData {
+	name: string;
+	money: number;
+	color: string;
+	legendFontColor: string;
+	legendFontSize: number;
+}
+
 function AnalysisScreen() {
 	const curTripInfo = useRecoilValue(CurTripInfoState);
+	const [paymentData, setPaymentData] = useState<charData[]>([]);
+	console.log(curTripInfo);
+
+	useFocusEffect(
+		useCallback(() => {
+			getData();
+		}, []),
+	);
+
+	const api = useAxiosWithAuth();
+	const getData = async () => {
+		const res = await api.get(`/analysis/${curTripInfo.id}`);
+		console.log(res.data);
+
+		const data: charData[] = res.data.list.map((item: any, index: number) => ({
+			name: item.member_name,
+			money: item.money,
+			color: `rgba(131, 167, 234, 1)`,
+			legendFontColor: '#7F7F7F',
+			legendFontSize: 15,
+		}));
+
+		console.log('data ', data);
+		setPaymentData(data);
+	};
+
+	const charConfig = {
+		backgroundColor: '#ffffff',
+		backgroundGradientFrom: '#fb8c00',
+		backgroundGradientTo: '#ffa726',
+		decimalPlaces: 2, // optional, defaults to 2dp
+		color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+		labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+		style: {
+			borderRadius: 16,
+		},
+		propsForDots: {
+			r: '6',
+			strokeWidth: '2',
+			stroke: '#ffa726',
+		},
+	};
+
 	return (
 		<View style={styles.viewContainer}>
 			<View style={styles.topView}>
@@ -18,6 +72,18 @@ function AnalysisScreen() {
 				<Text style={styles.info}>
 					{curTripInfo.startDay} ~ {curTripInfo.endDay}
 				</Text>
+			</View>
+			<View style={styles.chartView}>
+				<PieChart
+					data={paymentData}
+					width={Dimensions.get('window').width}
+					height={280}
+					backgroundColor="transparent"
+					chartConfig={charConfig}
+					accessor={'money'}
+					paddingLeft={'20'}
+					hasLegend={true}
+				/>
 			</View>
 		</View>
 	);
@@ -41,6 +107,11 @@ const styles = StyleSheet.create({
 	},
 	info: {
 		...TextStyles({ align: 'left', color: '#666666' }).small,
+	},
+	chartView: {
+		// backgroundColor: 'blue',
+		position: 'relative',
+		left: 80,
 	},
 });
 
