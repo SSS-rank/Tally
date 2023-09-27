ea1074d2c2984554b20f1b91654e07953cf0171e
rladbwls30 <rladbwls30@naver.com>
2023-09-27 14:16:59 +0900

    ✏️ fix: 계좌 등록 예외처리 및 css 수정
    Former-commit-id: 6631d8bb871c45254dd5fce4122eeea408ae7809

--- SSS-bank-Frontend/src/pages/AccountAdd.tsx
+++ SSS-bank-Frontend/src/pages/AccountAdd.tsx
@@ -102,24 +102,24 @@ function AccountAdd() {
 		checkPasswordMatch(pwd, e.target.value);
 	};
 
+	const isFormValid = bankName && pwd && confirmPassword && passwordMatch && pwd.length == 4;
+
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<Container component="main" maxWidth="xs">
 				<CssBaseline />
 				<Box
 					sx={{
-						marginTop: 8,
+						height: 'calc(100vh - 169px)',
 						display: 'flex',
 						flexDirection: 'column',
 						alignItems: 'center',
+						justifyContent: 'center',
 					}}
 				>
-					<Grid container spacing={2}>
-						<Grid item xs={4}>
-							<ArrowBackIosNewIcon onClick={goBack} />
-						</Grid>
-						<Grid>
-							<Typography component="h1" variant="h5">
+					<Grid container alignItems="center">
+						<Grid item flexGrow={1}>
+							<Typography component="h1" variant="h5" align="center">
 								계좌 추가
 							</Typography>
 						</Grid>
@@ -127,7 +127,7 @@ function AccountAdd() {
 					<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
 						<Grid container spacing={2}>
 							<Grid item xs={12}>
-								<Typography>은행 선택</Typography>
+								<Typography sx={{ marginBottom: '0.5rem' }}>은행 선택</Typography>
 								<Box
 									sx={{
 										display: 'flex',
@@ -154,9 +154,9 @@ function AccountAdd() {
 								</Box>
 							</Grid>
 							<Grid item xs={12}>
-								<TextField name="name" required fullWidth id="name" label="예금주" autoFocus />
-							</Grid>
-							<Grid item xs={12}>
+								<Typography sx={{ marginBottom: '0.5rem', marginTop: '1rem' }}>
+									사용할 비밀번호 4글자를 입력하세요.
+								</Typography>
 								<TextField
 									required
 									fullWidth
@@ -167,6 +167,7 @@ function AccountAdd() {
 									type="password"
 									value={pwd}
 									onChange={handlePasswordChange}
+									inputProps={{ maxLength: 4 }}
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -180,11 +181,12 @@ function AccountAdd() {
 									type="password"
 									value={confirmPassword}
 									onChange={handleConfirmPasswordChange}
+									inputProps={{ maxLength: 4 }}
 								/>
-								{confirmPassword && passwordMatch === true && (
+								{confirmPassword && passwordMatch === true && pwd.length == 4 && (
 									<Typography style={{ color: 'blue' }}>비밀번호가 일치합니다.</Typography>
 								)}
-								{confirmPassword && passwordMatch === false && (
+								{confirmPassword && passwordMatch === false && pwd.length == 4 && (
 									<Typography style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</Typography>
 								)}
 							</Grid>
@@ -193,6 +195,7 @@ function AccountAdd() {
 							type="submit"
 							fullWidth
 							variant="contained"
+							disabled={!isFormValid}
 							sx={{
 								mt: 3,
 								mb: 2,
