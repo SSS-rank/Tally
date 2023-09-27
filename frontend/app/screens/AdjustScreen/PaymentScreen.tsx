730fd64bd1a4812dfaa9c683c5a14c7d09ee49d8
kosigi19 <kosigi19@naver.com>
2023-09-27 10:42:46 +0900

    ⚡️ feat; 선택된 결제 계좌 확인 및 이체 페이지 네비게이션 연결
    Former-commit-id: 19631ad00af6d1750c1760d9b2429603fad81762

--- frontend/app/screens/AdjustScreen/PaymentScreen.tsx
+++ frontend/app/screens/AdjustScreen/PaymentScreen.tsx
@@ -7,23 +7,24 @@ import { useFocusEffect } from '@react-navigation/native';
 import PaymentAccountItem from '../../components/Adjust/PaymentAccountItem';
 import useAxiosWithAuth from '../../hooks/useAxiosWithAuth';
 import { Account } from '../../model/account';
+import { PayAdjustScreenProps } from '../../model/tripNavigator';
 import { TextStyles } from '../../styles/CommonStyles';
 
-const PaymentScreen = () => {
+const PaymentScreen = ({ navigation, route }: PayAdjustScreenProps) => {
 	const api = useAxiosWithAuth();
+	const { adjustId } = route.params;
 	const [accountList, setAccountList] = useState<Account[]>([]);
-	const [selectedAccountChange, setSelectedAccountChange] = useState('');
+	const [selectedAccount, setSelectedAccount] = useState('');
 
 	useFocusEffect(
 		useCallback(() => {
-			console.log('엥');
 			getAccountList();
 		}, []),
 	);
 
 	useEffect(() => {
 		const filteredAccounts = accountList.filter((item: Account) => {
-			if (item.accountNumber === selectedAccountChange) {
+			if (item.accountNumber === selectedAccount) {
 				item.representativeAccount = true;
 			} else {
 				item.representativeAccount = false;
@@ -31,13 +32,15 @@ const PaymentScreen = () => {
 			return item;
 		});
 		setAccountList(filteredAccounts);
-	}, [selectedAccountChange]);
+	}, [selectedAccount]);
 
 	const getAccountList = async () => {
 		try {
 			const res = await api.get(`/account`);
 			if (res.status === 200) {
 				setAccountList(res.data);
+				const account = res.data.find((item: Account) => item.representativeAccount === true);
+				if (account !== undefined) setSelectedAccount(account.accountNumber);
 			}
 		} catch (err) {
 			console.error(err);
@@ -59,13 +62,23 @@ const PaymentScreen = () => {
 						bankCode={item.bankCode}
 						bankName={item.bankName}
 						representativeAccount={item.representativeAccount}
-						setSelectedAccountChange={setSelectedAccountChange}
+						setSelectedAccountChange={setSelectedAccount}
 					/>
 				)}
 				keyExtractor={(item) => item.accountNumber}
 			/>
 			<View style={{ justifyContent: 'flex-end', flex: 1, marginVertical: 30 }}>
-				<Button mode="contained" buttonColor="#91C0EB" textColor="white">
+				<Button
+					mode="contained"
+					buttonColor="#91C0EB"
+					textColor="white"
+					onPress={() =>
+						navigation.navigate('Password', {
+							adjustId: adjustId,
+							accountNumber: selectedAccount,
+						})
+					}
+				>
 					완료
 				</Button>
 			</View>
