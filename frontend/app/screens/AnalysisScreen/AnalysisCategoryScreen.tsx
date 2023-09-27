341cffdd0a3d6ed629139d6d80323301c50607d9
zero9657@naver.com <zero9657@naver.com>
2023-09-27 11:20:04 +0900

    💄 style : 카테고리 별 상세 페이지 UI 개발
    Former-commit-id: 614683357d7ea6cf0b1cae41ccf66a596a805ac3

--- frontend/app/screens/AnalysisScreen/AnalysisCategoryScreen.tsx
+++ frontend/app/screens/AnalysisScreen/AnalysisCategoryScreen.tsx
@@ -1,9 +1,12 @@
 import React, { useCallback } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import { useFocusEffect } from '@react-navigation/native';
 
+import CategoryListItem from '../../components/AnalysisScreen/CategoryListItem';
+import DashLine from '../../components/DashLine';
 import { AnalysisCategoryScreenProps } from '../../model/tripNavigator';
+import { TextStyles } from '../../styles/CommonStyles';
 
 function AnalysisCategoryScreen({ navigation, route }: AnalysisCategoryScreenProps) {
 	useFocusEffect(
@@ -12,10 +15,23 @@ function AnalysisCategoryScreen({ navigation, route }: AnalysisCategoryScreenPro
 		}, []),
 	);
 	return (
-		<View>
-			<Text>상세 카테고리 페이지</Text>
+		<View style={styles.viewContainer}>
+			<Text style={styles.title}>25900원</Text>
+			<DashLine />
+			<CategoryListItem />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	viewContainer: {
+		flex: 1,
+		padding: 15,
+		backgroundColor: '#ffffff',
+	},
+	title: {
+		...TextStyles({ align: 'left', weight: 'bold', mBottom: 30 }).title,
+	},
+});
 
 export default AnalysisCategoryScreen;
