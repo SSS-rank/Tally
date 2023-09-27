f0a9126f02b9b596bb3c6a43ce8390c384f4a341
zero9657@naver.com <zero9657@naver.com>
2023-09-27 13:48:44 +0900

    ✨ feat : 동적으로 데이터 출력
    Former-commit-id: b46c3e4c3e8ac39aa4e1fc215e0e0c7197445b37

--- frontend/app/components/AnalysisScreen/CategoryListItem.tsx
+++ frontend/app/components/AnalysisScreen/CategoryListItem.tsx
@@ -2,38 +2,62 @@ import React from 'react';
 import { TouchableOpacity, View, StyleSheet, Text } from 'react-native';
 import { Avatar } from 'react-native-paper';
 
+import { categoryListItem } from '../../model/analysis';
 import { TextStyles } from '../../styles/CommonStyles';
 
-function CategoryListItem() {
+interface listItem extends categoryListItem {
+	category_id: number;
+}
+
+function CategoryListItem({
+	category_id,
+	payment_korea_date,
+	payment_uuid,
+	payment_title,
+	tag_member,
+	total_money,
+	my_money,
+}: listItem) {
 	return (
-		<TouchableOpacity style={styles.categoryItemView}>
-			<View style={styles.textView}>
-				<Avatar.Image
-					size={50}
-					style={styles.categoryImage}
-					source={{ uri: `https://sss-tally.s3.ap-northeast-2.amazonaws.com/category_${7}.png` }}
-				/>
+		<TouchableOpacity style={styles.categoryContainer}>
+			<View style={styles.categoryItemView}>
+				<View style={styles.textView}>
+					<Avatar.Image
+						size={50}
+						style={styles.categoryImage}
+						source={{
+							uri: `https://sss-tally.s3.ap-northeast-2.amazonaws.com/category_${category_id}.png`,
+						}}
+					/>
+					<View>
+						<Text style={styles.name}>{payment_title}</Text>
+						<Text style={styles.info}>{tag_member}</Text>
+						<Text style={{ marginLeft: 12 }}>{payment_korea_date}</Text>
+					</View>
+				</View>
 				<View>
-					<Text style={styles.name}>버스</Text>
-					<Text style={styles.info}>김싸피, 박싸피</Text>
+					<Text style={styles.text}>{my_money}원</Text>
+					<Text style={styles.totalMoney}>{total_money}원</Text>
 				</View>
 			</View>
-			<Text style={styles.text}>12000원</Text>
 		</TouchableOpacity>
 	);
 }
 
 const styles = StyleSheet.create({
+	categoryContainer: {
+		marginVertical: 14,
+	},
 	categoryItemView: {
 		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'space-between',
-		marginVertical: 14,
 	},
 	categoryImage: {
 		backgroundColor: 'transparent',
 	},
 	textView: {
+		flex: 1,
 		flexDirection: 'row',
 		alignItems: 'center',
 	},
@@ -44,7 +68,10 @@ const styles = StyleSheet.create({
 		...TextStyles({ align: 'left', mLeft: 10, color: '#666666' }).small,
 	},
 	text: {
-		...TextStyles({ align: 'left', mLeft: 10 }).regular,
+		...TextStyles({ align: 'left', mLeft: 10, weight: 'bold' }).regular,
+	},
+	totalMoney: {
+		...TextStyles({ align: 'right', mLeft: 10, color: '#666666' }).small,
 	},
 });
 
