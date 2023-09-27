de683f8b9d823f758ea4d46823a3d34d876b062d
박현철 <wns882@naver.com>
2023-09-27 16:00:12 +0900

    :zap: feat: Ocr 추가
    Former-commit-id: 9a824b76a7b797a818f399d0ef7308734c8abe85

---
+++ backend/src/main/java/com/sss/tally/api/ocr/controller/OcrController.java
@@ -0,0 +1,34 @@
+package com.sss.tally.api.ocr.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sss.tally.domain.ocr.service.OcrService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/ocr")
+public class OcrController {
+	private final OcrService ocrService;
+
+	@PostMapping("/upload")
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		System.out.println("file" + file);
+		File tempFile = File.createTempFile("temp", null);
+		file.transferTo(tempFile);
+		String aa = ocrService.Ocr(tempFile);
+		return ResponseEntity.status(HttpStatus.OK).body(aa);
+
+	}
+
+}
