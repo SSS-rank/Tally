5891e70aeb616dcd2d1a5991413100f3f0546cb7
zero9657@naver.com <zero9657@naver.com>
2023-09-28 15:59:06 +0900

    ✏️ fix : 스크린 title 동적 설정
    Former-commit-id: fda469bc4b498e42287c252b3a12104a57a093d7

--- frontend/app/screens/AnalysisScreen/AnalysisPersonalScreen.tsx
+++ frontend/app/screens/AnalysisScreen/AnalysisPersonalScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import { Text, View, StyleSheet, Dimensions, FlatList, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
 
 import { useFocusEffect } from '@react-navigation/native';
@@ -24,7 +24,7 @@ interface charData {
 
 function AnalysisPersonalScreen({ navigation, route }: AnalysisCategoryScreenProps) {
 	const curTripInfo = useRecoilValue(CurTripInfoState);
-	const { member_uuid } = route.params;
+	const { member_uuid, title } = route.params;
 	const [paymentData, setPaymentData] = useState<charData[]>([]);
 	const [list, setList] = useState<any[]>([]);
 	console.log('AnalysisPersonalScreen');
@@ -32,6 +32,7 @@ function AnalysisPersonalScreen({ navigation, route }: AnalysisCategoryScreenPro
 	const api = useAxiosWithAuth();
 	useFocusEffect(
 		useCallback(() => {
+			navigation.setOptions({ title: title + '님의 카테고리 별 지출' });
 			getPersonalData();
 		}, [member_uuid]),
 	);
@@ -117,22 +118,6 @@ function AnalysisPersonalScreen({ navigation, route }: AnalysisCategoryScreenPro
 					/>
 				))}
 			</View>
-			{/* <FlatList
-				data={list}
-				renderItem={({ item }) => (
-					<PersonalChartLegendItem
-						key={item.category_id}
-						category_id={item.category_id}
-						category_type={item.category_type}
-						money={item.money}
-						percent={item.percent}
-						color={item.color}
-						member_uuid={member_uuid}
-						navigation={navigation}
-					/>
-				)}
-				keyExtractor={(item) => item.member_uuid}
-			/> */}
 		</ScrollView>
 	);
 }
