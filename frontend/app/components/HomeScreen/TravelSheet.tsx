9f9dc88c93842c438ea93e362832165f680d63ed
zero9657@naver.com <zero9657@naver.com>
2023-09-30 16:39:15 +0900

    ✏️ fix : 여행지 추가 캐러셀 컴포넌트 적용
    Former-commit-id: 5c798e24267e041cf3b1b5cd215537c9534084c7

--- frontend/app/components/HomeScreen/TravelSheet.tsx
+++ frontend/app/components/HomeScreen/TravelSheet.tsx
@@ -6,6 +6,7 @@ import { Avatar, Button, Text } from 'react-native-paper';
 import RainAnimation from './RainAnimation';
 import SnowAnimation from './SnowAnimation';
 import SunnyAnimation from './SunnyAnimation';
+import TripAddCarouselItem from './TripAddCarouselItem';
 import { TravelSheetItem } from '../../model/mainTripItem';
 import { TextStyles } from '../../styles/CommonStyles';
 import { ViewStyles } from '../../styles/HomeStyles';
@@ -14,80 +15,95 @@ interface TravelSheetProps extends TravelSheetItem {
 	width: number;
 	color: string[];
 	weather: string;
+	travel_id: number;
 }
 
 function TravelSheet({ item }: { item: TravelSheetProps }) {
 	return (
-		<TouchableOpacity
-			style={{
-				...ViewStyles({ height: 300 }).box,
-				width: item.width,
-				marginHorizontal: 0,
-				elevation: 2,
-			}}
-		>
-			<LinearGradient colors={item.color} style={styles.linerContainer} />
-			{item.weather?.includes('Snow') && <SnowAnimation />}
-			{item.weather?.includes('Rain') && <RainAnimation />}
-			{item.weather?.includes('Sunny') && <SunnyAnimation />}
-
-			<View
-				style={{
-					flexDirection: 'row',
-					alignItems: 'center',
-					width: '100%',
-				}}
-			>
-				<View style={{ alignItems: 'flex-start', flex: 1 }}>
-					<View style={{ flexDirection: 'row', marginBottom: 5, alignItems: 'center' }}>
-						<Avatar.Image
-							size={32}
-							source={{
-								uri: `https://sss-tally.s3.ap-northeast-2.amazonaws.com/${item.travelLocation}.png`,
-							}}
-						/>
-						<Text style={TextStyles({ weight: 'bold', mLeft: 10 }).title}>
-							D - {item.remainDate}
-						</Text>
-					</View>
-					<View style={styles.titleView}>
-						<Text style={TextStyles({ weight: 'bold' }).title}>{item.travelTitle}</Text>
-						<Text style={TextStyles({ mLeft: 5 }).small}>{item.travelType}</Text>
-					</View>
-					<Text style={TextStyles({ mBottom: 5 }).small}>
-						{item.startDate} ~ {item.endDate}
-					</Text>
-					<Text style={TextStyles({ weight: 'bold' }).header}>{item.money}원</Text>
-				</View>
-			</View>
-			<View style={{ alignItems: 'flex-start', backgroundColor: 'red' }}></View>
-			<View style={ViewStyles().boxMate}>
-				{item.travelParticipants.map((member) => (
-					<Avatar.Image
-						key={member.member_uuid}
-						style={ViewStyles({ left: 0 }).avatarMate}
-						size={40}
-						source={{ uri: member.profile_image }}
-					/>
-				))}
-			</View>
-			<View
-				style={{
-					justifyContent: 'flex-end',
-					flex: 1,
-					// backgroundColor: 'red',
-				}}
-			>
-				<Button
-					icon="check"
-					mode="text"
-					// buttonColor="#000000"
-					onPress={() => console.log('Pressed')}
+		<>
+			{item.travel_id === undefined && (
+				<TouchableOpacity
+					style={{
+						...ViewStyles({ height: 300 }).box,
+						width: item.width,
+						marginHorizontal: 0,
+						elevation: 2,
+					}}
 				>
-					체크리스트
-				</Button>
-			</View>
-		</TouchableOpacity>
+					<LinearGradient colors={item.color} style={styles.linerContainer} />
+					{item.weather?.includes('Snow') && <SnowAnimation />}
+					{item.weather?.includes('Rain') && <RainAnimation />}
+					{item.weather?.includes('Sunny') && <SunnyAnimation />}
+
+					<View
+						style={{
+							flexDirection: 'row',
+							alignItems: 'center',
+							width: '100%',
+						}}
+					>
+						<View style={{ alignItems: 'flex-start', flex: 1 }}>
+							<View style={{ flexDirection: 'row', marginBottom: 5, alignItems: 'center' }}>
+								<Avatar.Image
+									size={32}
+									source={{
+										uri: `https://sss-tally.s3.ap-northeast-2.amazonaws.com/${item.travelLocation}.png`,
+									}}
+								/>
+								<Text style={TextStyles({ weight: 'bold', mLeft: 10 }).title}>
+									D - {item.remainDate}
+								</Text>
+							</View>
+							<View style={styles.titleView}>
+								<Text style={TextStyles({ weight: 'bold' }).title}>{item.travelTitle}</Text>
+								<Text style={TextStyles({ mLeft: 5 }).small}>{item.travelType}</Text>
+							</View>
+							<Text style={TextStyles({ mBottom: 5 }).small}>
+								{item.startDate} ~ {item.endDate}
+							</Text>
+							<Text style={TextStyles({ weight: 'bold' }).header}>{item.money}원</Text>
+						</View>
+					</View>
+					<View style={{ alignItems: 'flex-start', backgroundColor: 'red' }}></View>
+					<View style={ViewStyles().boxMate}>
+						{item.travelParticipants.map((member) => (
+							<Avatar.Image
+								key={member.member_uuid}
+								style={ViewStyles({ left: 0 }).avatarMate}
+								size={40}
+								source={{ uri: member.profile_image }}
+							/>
+						))}
+					</View>
+					<View
+						style={{
+							justifyContent: 'flex-end',
+							flex: 1,
+							// backgroundColor: 'red',
+						}}
+					>
+						<Button
+							icon="check"
+							mode="text"
+							// buttonColor="#000000"
+							onPress={() => console.log('Pressed')}
+						>
+							체크리스트
+						</Button>
+					</View>
+				</TouchableOpacity>
+			)}
+			{item.travel_id === -1 && (
+				<TripAddCarouselItem
+					containerStyle={{
+						...ViewStyles({ height: 300 }).box,
+						width: item.width,
+						marginHorizontal: 0,
+						elevation: 2,
+					}}
+				/>
+			)}
+		</>
 	);
 }
 
