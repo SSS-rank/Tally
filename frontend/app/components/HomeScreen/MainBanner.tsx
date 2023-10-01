6de64b626c6e7a87cc25f883455307b01981a7a4
kosigi19 <kosigi19@naver.com>
2023-10-02 05:44:28 +0900

    ✨ feat: 메인 배너 컴포넌트에 애니메이션 컴포넌트 추가
    Former-commit-id: 1a77b7f676e0cbb0b493919a2f22b16c4871ca5d

--- frontend/app/components/HomeScreen/MainBanner.tsx
+++ frontend/app/components/HomeScreen/MainBanner.tsx
@@ -6,6 +6,7 @@ import { useRecoilValue } from 'recoil';
 import { MemberState } from '../../recoil/memberRecoil';
 import { TextStyles } from '../../styles/CommonStyles';
 import { ViewStyles } from '../../styles/HomeStyles';
+import Bill from '../Animations/Bill';
 function MainBanner() {
 	const memberinfo = useRecoilValue(MemberState);
 	return (
@@ -16,12 +17,19 @@ function MainBanner() {
 				</Text>
 				님, 어디로 떠나시나요?
 			</Text>
-			<Text style={{ ...TextStyles({ align: 'left', weight: 'bold' }).header, marginVertical: 15 }}>
-				톡! 찍으면{'\n'}이체까지 딱!
-			</Text>
-			<Text style={TextStyles({ align: 'left' }).small}>
-				언제 어디서든{'\n'}바로 확인하고 정산하는{'\n'}간편한 여행
-			</Text>
+			<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+				<View style={{ flex: 1 }}>
+					<Text
+						style={{ ...TextStyles({ align: 'left', weight: 'bold' }).header, marginVertical: 15 }}
+					>
+						톡! 찍으면{'\n'}이체까지 딱!
+					</Text>
+					<Text style={TextStyles({ align: 'left' }).small}>
+						언제 어디서든{'\n'}바로 확인하고 정산하는{'\n'}간편한 여행
+					</Text>
+				</View>
+				<Bill />
+			</View>
 		</View>
 	);
 }
