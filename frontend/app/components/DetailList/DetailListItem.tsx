c45e833af962d24641704f5b126d9a96027fbcf0
zero9657@naver.com <zero9657@naver.com>
2023-10-02 01:26:06 +0900

    💄 style : 결제 내역 아이템 스타일 수정
    Former-commit-id: c01a2ff64fabfa4a419201f87c637a4570c6c412

--- frontend/app/components/DetailList/DetailListItem.tsx
+++ frontend/app/components/DetailList/DetailListItem.tsx
@@ -23,55 +23,49 @@ function DetailListItem({ item, navigation }: detailItemProps) {
 				})
 			}
 		>
-			<Text>{item.payment_date.split('T')[0]}</Text>
-			<View style={styles.detail_item_container}>
-				<View style={styles.detail_item_main}>
-					<View style={styles.title_status}>
-						<Text style={TextStyles().regular}>{item.payment_name}</Text>
-						{item.calculate_status != 'NONE' ? (
-							<DetailItemStatus status={item.calculate_status} />
-						) : null}
-						{!item.visible ? <Icon style={styles.lock} name="lock" /> : null}
-					</View>
+			<Text style={[styles.info, { marginBottom: 3 }]}>{item.payment_date.split('T')[0]}</Text>
+			<View style={styles.detail_item_main}>
+				<View style={styles.title_status}>
+					<Text style={styles.paymentName}>{item.payment_name}</Text>
+					{item.calculate_status != 'NONE' ? (
+						<DetailItemStatus status={item.calculate_status} />
+					) : null}
+					{!item.visible ? <Icon style={styles.lock} name="lock" /> : null}
+				</View>
 
-					<Text style={TextStyles().regular}>{item.amount}원</Text>
-				</View>
-				<View style={styles.detail_item_sub}>
-					<Text style={TextStyles().small}>{item.payment_date.split('T')[1]}</Text>
-					<Text style={TextStyles().small}>
-						{item.participants ? item.participants.join(',') : ''}
-					</Text>
-				</View>
+				<Text style={styles.money}>{item.amount}원</Text>
+			</View>
+			<View style={styles.detail_item_sub}>
+				<Text style={styles.info}>{item.payment_date.split('T')[1]}</Text>
+				<Text style={styles.info}>{item.participants ? item.participants.join(',') : ''}</Text>
 			</View>
 		</TouchableOpacity>
 	);
 }
 const styles = StyleSheet.create({
 	itemContainer: {
-		marginBottom: 10,
+		marginBottom: 20,
+	},
+	paymentName: {
+		...TextStyles({ align: 'left', weight: 'bold' }).title,
+	},
+	info: {
+		...TextStyles({ align: 'left', color: '#666666' }).small,
+	},
+	money: {
+		...TextStyles({ align: 'left', weight: 'bold' }).medium,
 	},
 	title_status: {
 		flexDirection: 'row',
 	},
-	detail_item_container: {
-		flexDirection: 'column',
-		padding: 10,
-	},
 	detail_item_main: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
+		marginBottom: 5,
 	},
 	detail_item_sub: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
-	},
-	chip: {
-		height: 30,
-		width: 75,
-		marginLeft: 10,
-		marginTop: 0,
-		marginBottom: 10,
-		justifyContent: 'center',
 	},
 	lock: {
 		marginLeft: 5,
