9fbeada8b736d3ba7de69760acd1b53e8894b47e
kosigi19 <kosigi19@naver.com>
2023-10-03 01:16:39 +0900

    💄 style: 알림, 설정 위치 변경
    Former-commit-id: 1a51652acfc04a6123316b7db8343ce2c970cc58

--- frontend/app/navigation/HomeStack.tsx
+++ frontend/app/navigation/HomeStack.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import AlertScreen from '../screens/AlertScreen/AlertScreen';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import AddAccountScreen from '../screens/SettingScreen/AddAccountScreen';
 import AuthAccountScreen from '../screens/SettingScreen/AuthAccountScreen';
 import ManageAccountScreen from '../screens/SettingScreen/ManageAccountScreen';
-import SettingScreen from '../screens/SettingScreen/SettingScreen';
 import TransferPasswrodConfirmScreen from '../screens/SettingScreen/TransferPasswrodConfirmScreen';
 import TransferPasswordScreen from '../screens/SettingScreen/TransferPasswrodScreen';
 
 export type HomeStackProps = {
 	Home: undefined;
-	Setting: undefined;
+	Alert: undefined;
 	Account: undefined;
 	AddAccount: undefined;
 	AuthAccount: undefined;
@@ -27,9 +27,9 @@ function HomeStack() {
 		<Stack.Navigator initialRouteName="Home" screenOptions={{ headerShadowVisible: false }}>
 			<Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
 			<Stack.Screen
-				name="Setting"
-				component={SettingScreen}
-				options={{ title: '설정', headerTitleAlign: 'center' }}
+				name="Alert"
+				component={AlertScreen}
+				options={{ title: '알림', headerTitleAlign: 'center' }}
 			/>
 			<Stack.Screen name="Account" component={ManageAccountScreen} options={{ title: '' }} />
 			<Stack.Screen name="AddAccount" component={AddAccountScreen} options={{ title: '' }} />
