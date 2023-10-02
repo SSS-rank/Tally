a8c39d071b79cfd31fc3468d0d8b452a0ef941c8
zero9657@naver.com <zero9657@naver.com>
2023-10-03 01:56:04 +0900

    ✏️ fix : AlertListItem 컴포넌트로 알림 리스트 조회 api 결과 받기
    Former-commit-id: 4414f42b8d7e9f003d0e9ab5eaf4fea88ff2d6ad

--- frontend/app/screens/AlertScreen/AlertScreen.tsx
+++ frontend/app/screens/AlertScreen/AlertScreen.tsx
@@ -1,19 +1,13 @@
 import React, { useCallback, useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
+import { StyleSheet, ScrollView } from 'react-native';
 
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { useFocusEffect } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-type TripStackProp = BottomTabScreenProps<MainTabsProps, 'TripStack'>;
+import AlertListItem from '../../components/AlertScreen/AlertListItem';
 import useAxiosWithAuth from '../../hooks/useAxiosWithAuth';
 import { AlertItem } from '../../model/alert';
-import { MainTabsProps } from '../../navigation/MainTabs';
-import { TripStackProps } from '../../navigation/TripStack';
-import { TextStyles } from '../../styles/CommonStyles';
 
-function AlertScreen({ navigation }: any) {
+function AlertScreen() {
 	const [alertList, setAlertList] = useState<AlertItem[]>([]);
 	useFocusEffect(
 		useCallback(() => {
@@ -27,7 +21,6 @@ function AlertScreen({ navigation }: any) {
 			const res = await api.get(`/notification`);
 
 			if (res.status === 200) {
-				console.log('getAlertList ', res.data);
 				setAlertList(res.data);
 			}
 		} catch (err: any) {
@@ -36,59 +29,16 @@ function AlertScreen({ navigation }: any) {
 	};
 
 	return (
-		<View style={styles.viewContainer}>
-			{/* <Text>💜❤️💛💚💙</Text> */}
-			{/* <View style={{ backgroundColor: 'white', paddingHorizontal: 15, width: '100%' }}>
-				<View style={{ flexDirection: 'row' }}>
-					<Text style={TextStyles().small}>💜</Text>
-					<Text style={TextStyles({ mLeft: 5 }).small}>정산</Text>
-					<Text style={{ ...TextStyles({ align: 'right', color: '#A0A0A0' }).small, flex: 1 }}>
-						9월 5일
-					</Text>
-				</View>
-				<View>
-					<Text style={TextStyles({ align: 'left', mLeft: 20, mTop: 10, mBottom: 10 }).regular}>
-						박싸피님이 80000원을 요청했어요. 정산 내역을 확인해보세요
-					</Text>
-				</View>
-			</View>
-			<View style={{ backgroundColor: 'white', paddingHorizontal: 15, width: '100%' }}>
-				<View style={{ flexDirection: 'row' }}>
-					<Text style={TextStyles().small}>💙</Text>
-					<Text style={TextStyles({ mLeft: 5 }).small}>이체</Text>
-					<Text style={{ ...TextStyles({ align: 'right', color: '#A0A0A0' }).small, flex: 1 }}>
-						9월 5일
-					</Text>
-				</View>
-				<View>
-					<Text style={TextStyles({ align: 'left', mLeft: 20, mTop: 10, mBottom: 10 }).regular}>
-						선택한 계좌의 잔액이 부족해요. {'\n'}SSS뱅크 111-1538--1234575
-					</Text>
-				</View>
-			</View>
-			<View style={{ backgroundColor: 'white', paddingHorizontal: 15, width: '100%' }}>
-				<View style={{ flexDirection: 'row' }}>
-					<Text style={TextStyles().small}>💛</Text>
-					<Text style={TextStyles({ mLeft: 5 }).small}>더치페이</Text>
-					<Text style={{ ...TextStyles({ align: 'right', color: '#A0A0A0' }).small, flex: 1 }}>
-						9월 5일
-					</Text>
-				</View>
-				<View>
-					<Text style={TextStyles({ align: 'left', mLeft: 20, mTop: 10, mBottom: 10 }).regular}>
-						김싸피님, 최싸피님이 'KTX'결제 건에 태그했어요! 어떤 내역인지 확인해보세요.
-					</Text>
-				</View>
-			</View> */}
-		</View>
+		<ScrollView style={styles.viewContainer}>
+			{alertList.map((item: AlertItem, index: number) => (
+				<AlertListItem key={index} item={item} />
+			))}
+		</ScrollView>
 	);
 }
 
 const styles = StyleSheet.create({
 	viewContainer: {
-		// flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
 		paddingTop: 10,
 		paddingHorizontal: 15,
 		backgroundColor: 'white',
