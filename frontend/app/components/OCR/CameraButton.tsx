272ec3443ee6a1697a17a355cddd51a14f73c9a7
이다곤 <leedagon624@gmail.com>
2023-10-03 04:58:01 +0900

    :sparkles: feat: ocr 처리를 위한 카메라, 앨범 접근 버튼, 모달 생성
    Former-commit-id: b863cfa69e3e572f164fd7f9ba5b345ae046cce6

---
+++ frontend/app/components/OCR/CameraButton.tsx
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import { View, Pressable, Platform, ImageURISource, Image } from 'react-native';
+import Config from 'react-native-config';
+import {
+	launchImageLibrary,
+	launchCamera,
+	CameraOptions,
+	ImageLibraryOptions,
+	ImagePickerResponse,
+} from 'react-native-image-picker';
+
+import axios from 'axios';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { OcrData } from '../../model/payment';
+import UploadModeModal from '../Modal/UploadModeModal';
+
+const imagePickerOption = {
+	mediaType: 'photo',
+	maxWidth: 768,
+	maxHeight: 768,
+	includeBase64: Platform.OS === 'android',
+};
+interface CameraButtonProps {
+	setOcrData: React.Dispatch<React.SetStateAction<OcrData | undefined>>;
+}
+function CameraButton({ setOcrData }: CameraButtonProps) {
+	const [img, setImg] = useState<ImageURISource>();
+
+	interface RawData {
+		infer_text: string;
+		name: string;
+	}
+
+	const formatData = (rawData: RawData[]): OcrData => {
+		const formattedData: OcrData = {
+			title: '',
+			amount: '',
+			date: '',
+		};
+
+		rawData.forEach((item) => {
+			switch (item.name) {
+				case 'title':
+					formattedData.title = item.infer_text;
+					break;
+				case 'amount':
+					formattedData.amount = item.infer_text;
+					break;
+				case 'date':
+					formattedData.date = item.infer_text;
+					break;
+				default:
+					break;
+			}
+		});
+
+		return formattedData;
+	};
+
+	const onPickImage = (res: ImagePickerResponse) => {
+		if (res.didCancel || !res) {
+			return;
+		}
+		if (res.assets) {
+			const formData = new FormData();
+			formData.append(
+				'message',
+				JSON.stringify({
+					version: 'V2',
+					requestId: 'string', //uuid로 아무 값 넣으면 된다123456
+					timestamp: 0, //현재 타임스탬프  근데 0이라도 써도 잘 나옴
+					images: [
+						{
+							format: 'jpg', //사진 포맷 데이터 형식 png, jpg 다 됨
+							name: 'test 1', //이미지 이름 res.assets[0].fileName 이거 넣으면 될듯??
+							// templateIds: [26521], //일본스벅 [26523], 일본 편의점 [26524]
+						},
+					],
+				}),
+			);
+			//리액트 네이티브 사진 파일
+			formData.append('file', {
+				uri: res.assets[0].uri,
+				type: res.assets[0].type,
+				name: res.assets[0].fileName,
+			});
+
+			axios
+				.post(
+					'https://dc4ulqm65h.apigw.ntruss.com/custom/v1/25277/e15a4310092ca2dce262ee02cb67f759c8194f3fa01d3508a353b57ca235656e/infer',
+					formData,
+					{
+						headers: {
+							'Content-Type': 'multipart/form-data',
+							'X-OCR-SECRET': Config.OCR_KEY,
+						},
+					},
+				)
+				.then((result) => {
+					if (result.status === 200) {
+						console.log('response:', result.data);
+						const extracted_data = result.data.images[0].fields.map((item: any) => {
+							return { infer_text: item.inferText, name: item.name };
+						});
+						console.log(extracted_data);
+						setOcrData(formatData(extracted_data));
+					}
+				})
+				.catch((e) => {
+					console.warn('requestWithBase64 error', e.response);
+				});
+
+			setImg({ uri: res.assets[0].uri });
+		}
+	};
+
+	const onLaunchCamera = () => {
+		launchCamera(imagePickerOption as CameraOptions, onPickImage);
+	};
+
+	const onLaunchImageLibrary = () => {
+		launchImageLibrary(imagePickerOption as ImageLibraryOptions, onPickImage);
+	};
+
+	const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+	const modalOpen = () => {
+		if (Platform.OS === 'android') {
+			setModalVisible(true);
+		} else {
+			// Handle iOS case if needed
+		}
+	};
+
+	return (
+		<>
+			<View>
+				<Pressable onPress={modalOpen}>
+					<Icon name="camera-alt" color="black" size={24} />
+				</Pressable>
+			</View>
+			<UploadModeModal
+				visible={modalVisible}
+				onClose={() => setModalVisible(false)}
+				onLaunchCamera={onLaunchCamera}
+				onLaunchImageLibrary={onLaunchImageLibrary}
+			/>
+		</>
+	);
+}
+
+export default CameraButton;
