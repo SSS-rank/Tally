13cb1123ae7cfa43a37163bfced7f10587946b18
kosigi19 <kosigi19@naver.com>
2023-10-03 00:55:39 +0900

    💄 style: 메인 문구 글자크기 조정
    Former-commit-id: 3331c786470889f0a38316e61ecb9ea898bc7f50

--- frontend/app/components/HomeScreen/MainBanner.tsx
+++ frontend/app/components/HomeScreen/MainBanner.tsx
@@ -11,8 +11,8 @@ function MainBanner() {
 	const memberinfo = useRecoilValue(MemberState);
 	return (
 		<View style={ViewStyles().banner}>
-			<Text style={TextStyles({ align: 'left' }).small}>
-				<Text style={TextStyles({ align: 'left', weight: 'bold' }).regular}>
+			<Text style={TextStyles({ align: 'left' }).regular}>
+				<Text style={TextStyles({ align: 'left', weight: 'bold' }).title}>
 					{memberinfo.nickname}
 				</Text>
 				님, 어디로 떠나시나요?
@@ -24,7 +24,7 @@ function MainBanner() {
 					>
 						톡! 찍으면{'\n'}이체까지 딱!
 					</Text>
-					<Text style={TextStyles({ align: 'left' }).small}>
+					<Text style={TextStyles({ align: 'left' }).regular}>
 						언제 어디서든{'\n'}바로 확인하고 정산하는{'\n'}간편한 여행
 					</Text>
 				</View>
