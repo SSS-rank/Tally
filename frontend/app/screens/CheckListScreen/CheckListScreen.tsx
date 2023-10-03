3d4245c7bafbe185deda9adebb8e6e294ed7751d
zero9657@naver.com <zero9657@naver.com>
2023-10-03 15:53:11 +0900

    ✨ feat : 기본 체크리스트 조회 api 연동
    Former-commit-id: 216bf2084ac2f6ee592085556c5f406de946c6ec

--- frontend/app/screens/CheckListScreen/CheckListScreen.tsx
+++ frontend/app/screens/CheckListScreen/CheckListScreen.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { Button } from 'react-native-paper';
+
+import { useFocusEffect } from '@react-navigation/native';
 
 import DashLine from '../../components/DashLine';
 import CheckListItem from '../../components/HomeScreen/CheckListItem';
+import useAxiosWithAuth from '../../hooks/useAxiosWithAuth';
+import { DefaultCheckListItem } from '../../model/checkList';
 import { CheckListScreenProps } from '../../model/homeNavigator';
 import { TextStyles } from '../../styles/CommonStyles';
 
 function CheckListScreen({ route }: CheckListScreenProps) {
 	const { travel_title, start_date, end_date } = route.params;
+	const [defaultCheckList, setDefaultCheckList] = useState<DefaultCheckListItem[]>([]);
+	const api = useAxiosWithAuth();
+
+	useFocusEffect(
+		useCallback(() => {
+			getDefaultCheckList();
+		}, []),
+	);
+
+	const getDefaultCheckList = async () => {
+		try {
+			const res = await api.get(`/default-checklist`);
+
+			if (res.status === 200) {
+				setDefaultCheckList(res.data);
+			}
+		} catch (err: any) {
+			console.error(err);
+		}
+	};
 	return (
 		<View style={styles.viewContainer}>
 			<View style={styles.topView}>
@@ -18,7 +43,16 @@ function CheckListScreen({ route }: CheckListScreenProps) {
 			</View>
 
 			<DashLine />
-			<CheckListItem />
+			<FlatList
+				data={defaultCheckList}
+				renderItem={({ item }) => (
+					<CheckListItem
+						default_check_list_id={item.default_check_list_id}
+						content={item.content}
+					/>
+				)}
+				keyExtractor={(item) => String(item.default_check_list_id)}
+			/>
 		</View>
 	);
 }
