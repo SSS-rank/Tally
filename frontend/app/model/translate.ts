5e9827fa1e925541e19119232761f4a4feb3ad04
zero9657@naver.com <zero9657@naver.com>
2023-10-06 02:12:38 +0900

    ✨ feat : 번역할 언어 데이터 생성
    Former-commit-id: 78f7f793d1f57c9a6c93c22bc51fb01e9006cd10

---
+++ frontend/app/model/translate.ts
@@ -0,0 +1,51 @@
+export interface TranslateInfo {
+	language: string;
+	language_code: string;
+}
+
+export const TranslateInfos: TranslateInfo[] = [
+	{
+		language: '한국어',
+		language_code: 'ko',
+	},
+	{
+		language: '영어',
+		language_code: 'en',
+	},
+	{
+		language: '일본어',
+		language_code: 'ja',
+	},
+	{
+		language: '중국어',
+		language_code: 'zh-CN',
+	},
+	{
+		language: '스페인어',
+		language_code: 'es',
+	},
+	{
+		language: '프랑스어',
+		language_code: 'fr',
+	},
+	{
+		language: '독일어',
+		language_code: 'de',
+	},
+	{
+		language: '러시아어',
+		language_code: 'ru',
+	},
+	{
+		language: '이탈리아어',
+		language_code: 'it',
+	},
+	{
+		language: '베트남어',
+		language_code: 'vi',
+	},
+	{
+		language: '태국어',
+		language_code: 'th',
+	},
+];
