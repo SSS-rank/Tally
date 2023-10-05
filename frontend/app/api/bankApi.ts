91c1e37bde181015092668d316a748b33a5a3a6e
이다곤 <leedagon624@gmail.com>
2023-10-05 13:44:47 +0900

    :pencil2: fix: 계좌 추가시 400에러 처리
    Former-commit-id: d194336af8d44632af3235484c5a1e0311c1783b

--- frontend/app/api/bankApi.ts
+++ frontend/app/api/bankApi.ts
@@ -1,3 +1,5 @@
+import { Alert } from 'react-native';
+
 import axios from 'axios';
 
 const bankApi = axios.create({
@@ -6,5 +8,22 @@ const bankApi = axios.create({
 		'Content-Type': 'application/json',
 	},
 });
+bankApi.interceptors.response.use(
+	function (response) {
+		return response;
+	},
+	function (error) {
+		const { config, response } = error;
+		if (response.status == 400) {
+			// console.log(config);
+			console.log(response.data.errorMessage);
+			console.log(response.data.Code);
+			Alert.alert(response.data.errorMessage);
+
+			console.log('응답결과:' + response.status);
+			console.log('메세지:' + response.message);
+		}
+	},
+);
 
 export default bankApi;
