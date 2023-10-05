21ce162f3a68ebf717d22e8c16bc263cffe54277
zero9657@naver.com <zero9657@naver.com>
2023-10-06 02:12:12 +0900

    💄 style  : LanguageItem 컴포넌트 생성
    Former-commit-id: 582dceded751ef2777ac017cdb5a14629e6ac2bb

---
+++ frontend/app/components/HomeScreen/LanguageItem.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { TranslateInfo } from '../../model/translate';
+import { TextStyles } from '../../styles/CommonStyles';
+
+interface LanguageItemProp {
+	language: string;
+	language_code: string;
+	cur_language_code: string;
+	setter: ({ language, language_code }: TranslateInfo) => void;
+	modalSetter: (status: boolean) => void;
+}
+
+function LanguageItem({
+	language,
+	language_code,
+	cur_language_code,
+	setter,
+	modalSetter,
+}: LanguageItemProp) {
+	console.log('laguage ', language, ' cur_language_code ', cur_language_code);
+
+	const handleOnPress = () => {
+		setter({ language, language_code });
+		modalSetter(false);
+	};
+	return (
+		<TouchableOpacity style={styles.itemContainer} onPress={handleOnPress}>
+			{language_code === cur_language_code && (
+				<>
+					<Text style={styles.textBold}>{language}</Text>
+					<Icon name="check" size={30} color="#91C0EB" />
+				</>
+			)}
+			{language_code !== cur_language_code && (
+				<>
+					<Text style={styles.text}>{language}</Text>
+				</>
+			)}
+		</TouchableOpacity>
+	);
+}
+
+const styles = StyleSheet.create({
+	itemContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 10,
+	},
+	text: {
+		...TextStyles({ align: 'left' }).regular,
+	},
+	textBold: {
+		...TextStyles({ align: 'left', weight: 'bold', color: '#91C0EB' }).regular,
+	},
+});
+
+export default LanguageItem;
